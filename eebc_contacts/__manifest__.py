bc3cf33fab60e71dc5451d64df05970c53b23dcf
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-08-16 09:42:16 -0700

    eebc_contacts:upload

---
+++ eebc_contacts/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : "EBBC Contactos",
+    'version' : '17.0.1.0',
+    'depends' : [],
+    'author' : "Quadro Soluciones",
+    'website': 'https://quadrosoluciones.com/',
+    'category': 'other',
+    'description': """
+        Este modulo conforma las configuraciones de los clientes y proveedores.
+
+    """,
+    "data": [
+    ],
+    "application": False,
+    "installable": True,
+    "license": "LGPL-3",
+}
