5d28bca92eb63e223178969c27ef4eeae7a22f77
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2024-08-16 17:41:52 -0700

    purchase_order_type

--- purchase_order_type/models/purchase_order_type.py
+++ purchase_order_type/models/purchase_order_type.py
@@ -5,7 +5,8 @@ logger = logging.getLogger(__name__)
 
 class PurchaseOrderType(models.Model):
     _name = "purchase.order.type"
-    _description = "Tipo de Orden de Compra"
+    _description = "Tipo de orden de compra"
 
-    name = fields.Char('Tipo de Orden de Compra', required=True)
+    name = fields.Char('Nombre', required=True)
     location_id = fields.Many2one('stock.location', string='Ubicación', required=True)
+    sequence_id = fields.Many2one('ir.sequence', string='Secuencia', required=True)
