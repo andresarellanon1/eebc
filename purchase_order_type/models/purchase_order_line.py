f76c0f539af330ccd2fdcd935e407b755e5caa66
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2024-08-19 16:59:20 -0700

    locations

--- purchase_order_type/models/purchase_order_line.py
+++ purchase_order_type/models/purchase_order_line.py
@@ -10,6 +10,6 @@ class PurchaseOrderLine(models.Model):
         res = super(PurchaseOrderLine, self)._prepare_stock_move_vals(picking, price_unit, product_uom_qty, product_uom)
 
         if self.order_id and self.order_id.purchase_order_type_id:
-            res['location_dest_id'] = self.purchase_order_type_id.location_id.id
+            res['location_dest_id'] = self.order_id.purchase_order_type_id.location_id.id
 
         return res
