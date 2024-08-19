b7b67268767f296c84ad412992838843fb9c5fbb
Jesus Alejos <57881931+jes-ale@users.noreply.github.com>
2024-08-19 15:11:22 -0700

    settings

---
+++ purchase_order_type/models/stock_picking.py
@@ -0,0 +1,14 @@
+from odoo import _, fields, models
+import logging
+
+logger = logging.getLogger(__name__)
+
+class StockPiking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.depends('picking_type_id', 'partner_id', 'purchase_id')
+    def _compute_location_id(self):
+        super(StockPiking, self)._compute_location_id()
+        for picking in self:
+            if picking.purchase_id and picking.purchase_id.purchase_order_type_id:
+                picking.location_dest_id = picking.purchase_id.purchase_order_type_id.location_id
