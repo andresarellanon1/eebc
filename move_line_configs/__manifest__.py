1c68a5cdb33b300effaa6278b2fe82cc92b03792
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-02 12:45:51 -0700

    fix

--- move_line_configs/__manifest__.py
+++ move_line_configs/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Product Line Configs",
-    "version": "17.0.0.1",
+    "version": "17.0.0.2",
     "depends": [],
     "author": "Quadro Soluciones",
     "website": "https://quadrosoluciones.com/",
@@ -9,8 +9,8 @@
         Este módulo conforma las configuraciones para las lineas de productos de forma global.
     """,
     "data": [
-        #"views/inherit_sale_order_form_view.xml",
-        #"views/inherit_account_move_form_view.xml",
+        "views/inherit_sale_order_form_view.xml",
+        "views/inherit_account_move_form_view.xml",
     ],
     "application": False,
     "installable": True,
