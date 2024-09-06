d3263746ef5feedde4e84f2613ced6072468f768
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-06 09:53:28 -0700

    priority_pricelist:fix

--- currency_selector_safe_margin/models/product_pricelist.py
+++ currency_selector_safe_margin/models/product_pricelist.py
@@ -3,4 +3,6 @@ from odoo import fields, models, api
 class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
 
-    location = fields.Many2one('res.company', string="Location", default=lambda self: self.env.user.company_id.id)
+    location = fields.Many2one('res.company', string="Location", 
+        default=lambda self: self.env.user.company_id.id,
+        domain=lambda self: [('parent_id', '=', self.env.user.company_id.id)])
