bb378b47531eafe3404c2d99ab4663a196314e2b
alejandro ramos <ivan.alj@hotmail.com>
2024-09-06 15:22:33 -0700

    tst comentario

--- currency_selector_safe_margin/models/product_pricelist.py
+++ currency_selector_safe_margin/models/product_pricelist.py
@@ -3,4 +3,4 @@ from odoo import fields, models, api
 class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
 
-    location = fields.Many2one('res.company', string="Location", default=lambda self: self.env.user.company_id.id)
+    # location = fields.Many2one('res.company', string="Location", default=lambda self: self.env.user.company_id.id)
