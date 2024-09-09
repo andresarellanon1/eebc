3fbe6cc59e18ec668d5e6a4a8aa6f3b4ba8ee256
alejandro ramos <ivan.alj@hotmail.com>
2024-09-09 09:34:54 -0700

    test cambio vendedor

--- sale_order_action/models/sale_order.py
+++ sale_order_action/models/sale_order.py
@@ -9,15 +9,15 @@ class SaleOrder(models.Model):
 
     _inherit = 'sale.order'
 
-    user_id = fields.Many2one(
-        comodel_name='res.users',
-        string="Salesperson",
-        compute='_compute_user_id',
-        store=True, readonly=False, precompute=True, index=True,
-        tracking=2,
-        domain=lambda self: "[('groups_id', '=', {}), ('share', '=', False), ('company_ids', '=', company_id)]".format(
-            self.env.ref("sales_team.group_sale_salesman").id
-        ))
+    # user_id = fields.Many2one(
+    #     comodel_name='res.users',
+    #     string="Salesperson",
+    #     compute='_compute_user_id',
+    #     store=True, readonly=False, precompute=True, index=True,
+    #     tracking=2,
+    #     domain=lambda self: "[('groups_id', '=', {}), ('share', '=', False), ('company_ids', '=', company_id)]".format(
+    #         self.env.ref("sales_team.group_sale_salesman").id
+    #     ))
 
     @api.model
     def cancelar_cotizaciones_vencidas(self):
@@ -25,9 +25,9 @@ class SaleOrder(models.Model):
         for order in quotations:
             order.action_cancel()
 
-    @api.depends('partner_id')
-    def _compute_user_id(self):
-        _logger.warning('Es nuestro compute')
-        for order in self:
-            # Asigna siempre al usuario logueado como el user_id
-            order.user_id = self.env.user
+    # @api.depends('partner_id')
+    # def _compute_user_id(self):
+    #     _logger.warning('Es nuestro compute')
+    #     for order in self:
+    #         # Asigna siempre al usuario logueado como el user_id
+    #         order.user_id = self.env.user
