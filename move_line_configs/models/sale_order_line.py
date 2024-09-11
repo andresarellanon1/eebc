141e873821ffd6129400abc4bdbc49dc19301d40
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-11 13:09:31 -0700

    Update sale_order_line.py

--- move_line_configs/models/sale_order_line.py
+++ move_line_configs/models/sale_order_line.py
@@ -5,4 +5,3 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     complete_description = fields.Char(string='Large description', default=lambda self: self.name)
-    use_large_description = fields.Boolean(string='Usar descripción larga')
