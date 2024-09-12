b5be19d13b2d1a323dc74ad3d343e197facf12be
Aybancho <ivan.rosas@uabc.edu.mx>
2024-09-12 12:01:17 -0700

    test app menu v6

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.10',
+    'version': '17.0.1.11',
     'depends': ["stock"],
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
