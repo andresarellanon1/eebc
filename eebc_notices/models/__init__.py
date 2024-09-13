394b7273cfe79223d445fdc2cde7f82aa7f08672
alejandro ramos <ivan.alj@hotmail.com>
2024-09-13 10:56:29 -0700

    TEST UPDATE FIELDS V1

--- eebc_notices/models/__init__.py
+++ eebc_notices/models/__init__.py
@@ -1,3 +1,4 @@
 from . import notices
 from . import purchase_order
 from . import account_move
+from . import notices_lines
--- eebc_notices/models/notices.py
+++ eebc_notices/models/notices.py
@@ -5,12 +5,7 @@ class Notices(models.Model):
     _name= 'notices.notices'
 
     resource = fields.Float()
-    order = fields.One2many(
-        string='Orden de compra origen',
-        comodel_name='purchase.order',
-        inverse_name='purchase_order_notice_id',
-    )
-    
+ 
     supplier = fields.Char( string='Proveedor')
     folio = fields.Char(string='Folio')
     create_date = fields.Date(
@@ -18,12 +13,31 @@ class Notices(models.Model):
         default=fields.Date.context_today,
     )
     
-    invoices = fields.One2many(
-        string='Facturas',
-        comodel_name='account.move',
-        inverse_name='account_move_notice_id',
-    )
+  
     notice = fields.Char(string='Aviso')
     description = fields.Char(string='Descripción')
     quantity = fields.Float(string='Cantidad')
     series = fields.Char(string='Series (s)')
+
+     # Relación One2many con órdenes de compra
+    order_ids = fields.One2many(
+        comodel_name='purchase.order',
+        inverse_name='notice_id',
+        string='Órdenes de compra'
+    )
+    
+    # Relación Many2many con facturas (a través de las órdenes de compra)
+    invoice_ids = fields.Many2many(
+        comodel_name='account.move',
+        string='Facturas',
+        compute='_compute_invoices'
+    )
+
+    @api.depends('order_ids')
+    def _compute_invoices(self):
+        for record in self:
+            invoices = self.env['account.move'].search([('invoice_origin', 'in', record.order_ids.mapped('name'))])
+            record.invoice_ids = invoices
+
+
+ 
