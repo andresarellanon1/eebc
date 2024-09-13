5bd70d7684c60cf25b9877b02861696f440bce21
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-13 11:18:26 -0700

    fix domain

--- product_supplier/models/purchase_order_line.py
+++ product_supplier/models/purchase_order_line.py
@@ -3,21 +3,13 @@ from odoo import fields, models, api
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
+    product_ids = fields.Many2many('product.product')
+
     @api.onchange('order_id.partner_id')
     def _onchange_partner_id(self):
         if self.order_id.partner_id:
-            product_templates = self.env['product.template'].search([('partner_id', '=', self.order_id.partner_id.id)])
+            products = self.env['product.supplierinfo'].search([('name', '=', self.order_id.partner_id.id)]).mapped('product_tmpl_id')
             
-            product_ids = self.env['product.product'].search([('product_tmpl_id', 'in', product_templates.ids)]).ids
+            product_ids = self.env['product.product'].search([('product_tmpl_id', 'in', products.ids)]).ids
             
-            return {
-                'domain': {
-                    'product_id': [('id', 'in', product_ids)]
-                }
-            }
-        else:
-            return {
-                'domain': {
-                    'product_id': []
-                }
-            }
+            self.product_ids = [(6, 0, product_ids)]
