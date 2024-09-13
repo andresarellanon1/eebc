b01ad1002410ac395bb8803278907281c70dcdde
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-13 13:24:43 -0700

    Update purchase_order.py

--- product_supplier/models/purchase_order.py
+++ product_supplier/models/purchase_order.py
@@ -10,4 +10,5 @@ class PurchaseOrder(models.Model):
         if self.partner_id:
             products = self.env['product.supplierinfo'].search([('partner_id', '=', self.partner_id.id)]).mapped('product_tmpl_id')
             
+            logger.warning(f'1 {products.ids}')
             self.order_line.product_template_id = [(6, 0, products)]
