d2864f50d2ebe5853eedea04e3669e4b23b5ea88
alejandro ramos <ivan.alj@hotmail.com>
2024-09-13 16:17:53 -0700

    TEST UPDATE FIELDS V2

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.16',
+    'version': '17.0.1.17',
     'depends': ["stock"],
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
