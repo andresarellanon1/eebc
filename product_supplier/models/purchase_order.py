8674ed6bc6b6880b6917fbd22e768b15809c6fb0
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-13 12:10:51 -0700

    Update purchase_order.py

--- product_supplier/models/purchase_order.py
+++ product_supplier/models/purchase_order.py
@@ -10,7 +10,7 @@ class PurchaseOrder(models.Model):
         if self.partner_id:
             products = self.env['product.supplierinfo'].search([('id', '=', self.partner_id.id)]).mapped('product_tmpl_id')
             
-            logger.warning(f'1 {products}')
+            logger.warning(f'1 {products.ids}')
             product_ids = self.env['product.product'].search([('product_tmpl_id', 'in', products.ids)])
             
             logger.warning(f'2 {product_ids}')
