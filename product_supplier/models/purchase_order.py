75658f3d380e40b1cb5069d6d16da6e2d9ee42ef
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-13 14:03:57 -0700

    asas

--- product_supplier/models/purchase_order.py
+++ product_supplier/models/purchase_order.py
@@ -5,10 +5,11 @@ logger = logging.getLogger(__name__)
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
+    supplier_products_ids = fields.Many2many('product.template', string='Supplier Products')
+
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         if self.partner_id:
-            products = self.env['product.supplierinfo'].search([('partner_id', '=', self.partner_id.id)]).mapped('product_tmpl_id')
-            
-            logger.warning(f'1 {products.ids}')
-            return {'domain': {'order_line.product_template_id': [('product_tmpl_id', 'in', products.ids)]}}
+            supplier_products = self.env['product.supplierinfo'].search([('partner_id', '=', self.partner_id.id)]).mapped('product_tmpl_id')
+            logger.warning(f'1 {supplier_products.ids}')
+            self.supplier_products_ids = supplier_products
