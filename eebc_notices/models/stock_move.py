f2378f29ec778ad85be0c05ac092e21d0501b116
alejandro ramos <ivan.alj@hotmail.com>
2024-09-18 15:52:43 -0700

    test invisible button v1

--- eebc_notices/models/stock_move.py
+++ eebc_notices/models/stock_move.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -14,5 +14,20 @@ class StockMove(models.Model):
         
         _logger.warning('Entramos a metodo de crear aviso')
         
-        
-        
+
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    has_aviso_in_attributes = fields.Boolean(
+        string="Tiene 'aviso' en atributos",
+        compute='_compute_has_aviso_in_attributes'
+    )
+
+    @api.depends('move_ids_without_package.product_id.attribute_line_ids')
+    def _compute_has_aviso_in_attributes(self):
+        for record in self:
+            record.has_aviso_in_attributes = any(
+                'aviso' in attr.name for move in record.move_ids_without_package for attr in move.product_id.attribute_line_ids.mapped('attribute_id')
+            )
