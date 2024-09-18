da60c86e119252f3a6acfb719cf8885d8ba98baa
alejandro ramos <ivan.alj@hotmail.com>
2024-09-18 16:06:35 -0700

    test invisible button v2

--- eebc_notices/models/stock_move.py
+++ eebc_notices/models/stock_move.py
@@ -6,6 +6,19 @@ _logger = logging.getLogger(__name__)
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
+
+
+    has_aviso_in_attributes = fields.Boolean(
+        string="Tiene 'aviso' en atributos",
+        compute='_compute_has_aviso_in_attributes'
+    )
+
+    @api.depends('product_id.attribute_line_ids')
+    def _compute_has_aviso_in_attributes(self):
+        for move in self:
+            move.has_aviso_in_attributes = any(
+                'aviso' in attr.name for attr in move.product_id.attribute_line_ids.mapped('attribute_id')
+            )
     
     
     def create_notice(self):
@@ -13,21 +26,7 @@ class StockMove(models.Model):
         # notice = self.env['notices.notices'].
         
         _logger.warning('Entramos a metodo de crear aviso')
+
         
 
 
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    has_aviso_in_attributes = fields.Boolean(
-        string="Tiene 'aviso' en atributos",
-        compute='_compute_has_aviso_in_attributes'
-    )
-
-    @api.depends('move_ids_without_package.product_id.attribute_line_ids')
-    def _compute_has_aviso_in_attributes(self):
-        for record in self:
-            record.has_aviso_in_attributes = any(
-                'aviso' in attr.name for move in record.move_ids_without_package for attr in move.product_id.attribute_line_ids.mapped('attribute_id')
-            )
