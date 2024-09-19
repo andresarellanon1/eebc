c4354dcba9bdc1be76753a91692c170baef1a2da
Aybancho <ivan.rosas@uabc.edu.mx>
2024-09-19 12:27:27 -0700

    test create notice v2

--- eebc_notices/models/stock_move.py
+++ eebc_notices/models/stock_move.py
@@ -27,18 +27,21 @@ class StockMove(models.Model):
         _logger.warning('Entramos al método de crear aviso para el movimiento: %s', self.id)
 
         # Puedes acceder a los datos de la línea (stock.move) que recibió el clic
-        _logger.warning('Producto: %s', self.product_id.name)
+        _logger.warning('Producto: %s', self.product_id)
         _logger.warning('Cantidad: %s', self.product_uom_qty)
 
         # Crear el aviso relacionado a partir de los datos del movimiento
         notice_vals = {
             'description': 'Aviso generado para el producto %s en la operación %s' % (self.product_id.name, self.picking_id.name),
             'quantity': self.product_uom_qty,
+            # 'resource': 
             # Otros valores que quieras pasar a notices.notices
         }
+        _logger.warning('VALS PARA AVISO: %s', notice_vals)
         
-        notice = self.env['notices.notices'].create(notice_vals)
-        _logger.warning('Se ha creado el aviso con ID: %s', notice.id)
+        
+        # notice = self.env['notices.notices'].create(notice_vals)
+        # _logger.warning('Se ha creado el aviso con ID: %s', notice.id)
         
 
 
