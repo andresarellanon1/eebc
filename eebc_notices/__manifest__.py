ec6d4c18cc3d399b76092c4d69736f5baa134809
Aybancho <ivan.rosas@uabc.edu.mx>
2024-09-19 09:28:57 -0700

    test invisible button v1

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.27',
+    'version': '17.0.1.28',
     'depends': ["stock"],
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
