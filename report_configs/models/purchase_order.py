cce6cde72a358b839ea381da415da9595393a2ab
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-09-20 09:55:50 -0700

    update

--- report_configs/models/purchase_order.py
+++ report_configs/models/purchase_order.py
@@ -4,7 +4,11 @@ class AccountMove(models.Model):
 
     _inherit = 'purchase.order'
 
-    @api.print_quotation
+    def print_quotation(self):
+        self.write({'state': "sent"})
+        return self.env.ref('report_configs.out_purchaseorder_template_custom').report_action(self)
+
+
     def _get_name_invoice_report(self):
         self.ensure_one()
         return 'report_configs.out_purchaseorder_template_custom'
