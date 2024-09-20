f63f662518cf083533e94afaf34c2630eec996c9
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-09-20 09:53:47 -0700

    update

--- report_configs/models/purchase_order.py
+++ report_configs/models/purchase_order.py
@@ -4,6 +4,7 @@ class AccountMove(models.Model):
 
     _inherit = 'purchase.order'
 
+    @api.print_quotation
     def _get_name_invoice_report(self):
         self.ensure_one()
         return 'report_configs.out_purchaseorder_template_custom'
