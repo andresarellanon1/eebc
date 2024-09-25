64108ec17356c475d9c4efffb8039cf77bd3df0d
alejandro ramos <ivan.alj@hotmail.com>
2024-09-25 16:23:31 -0700

    Test qty color v1

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.50',
+    'version': '17.0.1.51',
     'depends': ["stock"],
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
