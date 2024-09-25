e74e2a77d2c0c20d93f9086b17cee6d4412089c2
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-25 13:33:01 -0700

    Update project_task.py

--- stock_picking_projects/models/project_task.py
+++ stock_picking_projects/models/project_task.py
@@ -5,6 +5,6 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     stock_ids = fields.One2many('stock.picking', 'project_id', string="stock")
-    move_ids = fields.One2many('stock.move', string="Lineas de operaciones")
+    move_ids = fields.One2many('stock.move', 'task_id', string="Lineas de operaciones")
 
     
