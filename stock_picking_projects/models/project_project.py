c8c200b61891c3adc05104cee43d6d7864a54dc3
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-30 11:43:57 -0700

    fix4

--- stock_picking_projects/models/project_project.py
+++ stock_picking_projects/models/project_project.py
@@ -8,6 +8,15 @@ class ProjectProject(models.Model):
     pickin_ids = fields.Many2many(
         'stock.picking', 
         string="Operaciones de Inventario",
+        compute="_compute_pickin_ids"
         )
     
-    
+    @api.depends('id')
+    def _compute_pickin_ids(self):
+        for project in self:
+            project_tasks = self.env['project.task'].search([('project_id', '=', project.id)])
+            task_picking_ids = self.env['stock.picking'].search([
+                ('project_id', 'in', project_tasks.ids)
+            ])
+            
+            project.pickin_ids = task_picking_ids
