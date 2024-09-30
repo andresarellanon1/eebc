9a073ced1c0bc4b748fde801f51e22e3ad2b263c
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-09-30 10:32:20 -0700

    c6

--- stock_picking_projects/models/project_project.py
+++ stock_picking_projects/models/project_project.py
@@ -5,3 +5,20 @@ class ProjectProject(models.Model):
     _inherit = 'project.project'
 
     default_picking_type_id = fields.Many2one('stock.picking.type', string="Operation type", required=True)
+    pickin_ids = fields.Many2many(
+        'stock.picking', 
+        compute="_compute_pickin_ids", 
+        string="Operaciones de Inventario", 
+        domain="[('id', 'in', pickin_ids)]"
+    )
+
+    @api.depends('id')
+    def _compute_pickin_ids(self):
+        for project in self:
+            project_tasks = self.env['project.task'].search([('project_id', '=', project.id)])
+            
+            task_picking_ids = self.env['stock.picking'].search([
+                ('task_id', 'in', project_tasks.ids)
+            ])
+            
+            project.pickin_ids = task_picking_ids
