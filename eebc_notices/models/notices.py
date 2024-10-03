c67cea669ffe4c00e2fa90a413bb5972d6812b6e
Aybancho <ivan.rosas@uabc.edu.mx>
2024-10-03 12:37:13 -0700

    test folio hisotrial

--- eebc_notices/models/notices.py
+++ eebc_notices/models/notices.py
@@ -1,7 +1,7 @@
 from odoo import fields, models, api
 # TODO: Pendiente validaciones de readonly en campos
 
-# campo nuevo total computada(sumatoria de la cantidad de historiales)
+# campo nuevo total computada(sumatoria de la cantidad de historiales) - lISTO!!
 
 class Notices(models.Model):
 
