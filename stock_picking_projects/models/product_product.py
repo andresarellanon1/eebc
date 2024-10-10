9251e65213bff9fd8c04cb42e8052a0f5aabde73
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-10-10 12:07:07 -0700

    update inherit_timesheet_view_form.xml

--- stock_picking_projects/models/product_product.py
+++ stock_picking_projects/models/product_product.py
@@ -3,4 +3,5 @@ from odoo import models, fields
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    project_id = fields.Many2one('project.project', string='Project')
+    project_id = fields.Many2one('project.project', string='Proyecto')
+    quantity = fields.Integer(string='Cantidad')
--- stock_picking_projects/models/stock_picking.py
+++ stock_picking_projects/models/stock_picking.py
@@ -5,3 +5,4 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     task_id = fields.Many2one('project.task', string='projects')
+    project_id = fields.Many2one('project.project', string='Proyecto')
