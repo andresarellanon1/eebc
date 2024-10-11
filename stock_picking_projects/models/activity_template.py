2874b49d166712bac1c005e0dc8bdd066d40b97a
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-11 10:48:32 -0700

    update

---
+++ stock_picking_projects/models/activity_template.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class ActivityTemplate(models.Model):
+
+    _name = 'activity.template'
+
+    name = fields.Char(string="Nombre", store=True)
+    description = fields.Char(string="Descripción", store=True)
+    allocated_hours = fields.Float(string="Horas", store=True)
+
+    project_id = fields.Many2one(
+        'project.project',  # Referencia al modelo
+        string='Actividad',
+        store = True,
+        copied = True)
+
+    
