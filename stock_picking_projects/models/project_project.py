ddf60c3766c97026de781a1d5d8a951a7de86975
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-16 13:03:39 -0700

    update

--- stock_picking_projects/models/project_project.py
+++ stock_picking_projects/models/project_project.py
@@ -6,20 +6,28 @@ class ProjectProject(models.Model):
 
     default_picking_type_id = fields.Many2one('stock.picking.type', string="Operation type", required=True)
     pickin_ids = fields.Many2many('stock.picking', string="Operaciones de Inventario")
-    product_ids = fields.One2many('product.product', 'project_id', string='Products')
+    
+    product_ids = fields.One2many(
+        'product.product', 
+        'project_id', 
+        string='Products'
+    )
 
     activities_tmpl_id = fields.Many2one(
         'activity.template',  # Referencia al modelo
         string='Plantilla de actividades'
     )
 
-    # actividad_id = fields.Many2one(
-    #     'project.project',  # Referencia al mismo modelo
-    #     string='Actividad',
-    #     store = True,
-    #     copied = True
-    # )
+    line_activities_ids = fields.One2many(
+        'line.activities',  # Referencia al modelo
+        'project_id',
+        string='Lineas de actividades'
+    )
     
+    @api.onchange('activities_tmpl_id')
+    def _onchange_activities_tmpl_id(self):
+        for record in self:
+            record.line_activities_ids = record.activities_tmpl_id.line_activities_ids
 
     @api.depends('task_id.stock_ids')
     def _compute_pickin_ids(self):
