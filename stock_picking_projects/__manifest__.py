613463f637206fa158952038caf881ae8e7bc8d8
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-10-18 14:10:55 -0700

    big dump

--- stock_picking_projects/__manifest__.py
+++ stock_picking_projects/__manifest__.py
@@ -13,7 +13,6 @@
         "views/inherit_project_task_view_form.xml",
         "views/view_project_project_kanban.xml",
         "views/inherit_project_view_form.xml",
-        "views/inherit_edit_project_view.xml",
         "views/inherit_pickin_form_view.xml",
         "views/inherit_timesheet_view_form.xml",
         "views/inherit_timesheet_line_tree.xml",
