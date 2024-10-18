6afe9abbfe920d20e171dba9c6ba14fca99cbdac
enriquequadro <enrique@quadrosoluciones.mx>
2024-10-18 10:27:04 -0700

    bid_date

--- stock_picking_projects/models/project_project.py
+++ stock_picking_projects/models/project_project.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+import datetime
 
 class ProjectProject(models.Model):
 
@@ -6,6 +7,8 @@ class ProjectProject(models.Model):
 
     default_picking_type_id = fields.Many2one('stock.picking.type', string="Operation type", required=True)
     pickin_ids = fields.Many2many('stock.picking', string="Operaciones de Inventario")
+    bid_date = fields.Date(string="Fecha de licitación")
+    bid_string = fields.String(string="Clave de licitacion", readonly=True)
     
     product_ids = fields.One2many(
         'product.product', 
@@ -23,6 +26,18 @@ class ProjectProject(models.Model):
         'project_id',
         string='Lineas de actividades'
     )
+
+    @api.onchange('bid_date')
+    def bid_date_onchange_(self):
+        for record in self:
+            code = record.id
+            date = datetime.strptime(record.bid_date, "%Y-%m-%d")
+            day = f"{date.day:02}"
+            month = f"{date.month:02}"
+            year = f"{date.year % 100:02}"
+
+            record.bid_string = "{day}{month}{year}{code}"
+
     
     @api.onchange('activities_tmpl_id')
     def _onchange_activities_tmpl_id(self):
@@ -32,6 +47,6 @@ class ProjectProject(models.Model):
     @api.depends('task_id.stock_ids')
     def _compute_pickin_ids(self):
         for record in self:
-            record.pickin_ids = record.task_id.stock_ids
+            record.pickin_ids = record.task_id.stock_ids()
 
     
