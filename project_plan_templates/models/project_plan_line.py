65415ce8dd462f27e3ac27785e7ce85beed6aad8
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-10-21 12:22:15 -0700

    c2

--- project_plan_templates/models/project_plan_line.py
+++ project_plan_templates/models/project_plan_line.py
@@ -5,7 +5,7 @@ class ProjectLines(models.Model):
 
     _name = 'project.plan.line'
     _description = 'Project plan lines'
-
+ 
     name = fields.Char(string="Name")
     chapter = fields.Char(string="Chapter", required=True)
     clave = fields.Integer(string="Task id")
