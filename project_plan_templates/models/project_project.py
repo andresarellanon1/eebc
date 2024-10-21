d42929c8541c500873c642cc9d27ec8a169c38b9
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-10-21 12:44:37 -0700

    c6

--- project_plan_templates/models/project_project.py
+++ project_plan_templates/models/project_project.py
@@ -5,9 +5,9 @@ class ProjectProject(models.Model):
 
     _inherit = 'project.project'
     
-    project_plan_id = fields.One2many('project.plan', 'project_id', string="Project template")
+    project_plan_id = fields.Many2one('project.plan', string="Project template")
     project_plan_description = fields.Char(string="Project description")
-    project_plan_lines = fields.One2many('project.plan.line', 'project_id', string="Project plan lines")
+    project_plan_lines = fields.One2many('project.plan.line', 'origin_project_id', string="Project plan lines")
 
     @api.depends('project_plan_id')
     def plan_lines(self):
