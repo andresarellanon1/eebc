644efeed564ca497051f3dea0f4ac8b4898dbfbf
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-10-21 12:23:24 -0700

    Update project_project.py

--- project_plan_templates/models/project_project.py
+++ project_plan_templates/models/project_project.py
@@ -7,14 +7,6 @@ class ProjectProject(models.Model):
     
     project_plan_id = fields.One2many('project.plan', 'project_id', string="Project template")
     project_plan_description = fields.Char(string="Project description")
-    project_plan_lines = fields.One2many('project.plan.line', 'project_id', string="Project plan lines")
+    #project_plan_lines = fields.One2many('project.plan.line', 'project_id', string="Project plan lines")
 
-    @api.depends('project_plan_id')
-    def plan_lines(self):
-        for project in self:
-            if project.project_plan_id:
-                project.project_plan_lines = [(6, 0, project.project_plan_id.plan_line_ids.ids)]
-                project.project_plan_description = project.project_plan_id.description
-            else:
-                project.project_plan_lines = [(5, 0, 0)]
-                project.project_plan_description = False
+    
