252a08ef37be1c53340450deb3893557d6e00e55
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-21 10:39:13 -0700

    update

--- stock_picking_projects/models/stock_picking.py
+++ stock_picking_projects/models/stock_picking.py
@@ -6,3 +6,4 @@ class StockPicking(models.Model):
 
     task_id = fields.Many2one('project.task', string='projects')
     project_id = fields.Many2one('project.project', string='Proyecto')
+    new_selection = fields.Selection(string='Nueva selección', copy=False, selection=[('estimacion','Estimación'),('traslado','Traslado')])
