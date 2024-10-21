7cfaf0ea0a3ca0f928e764cac2bb7075d95797f8
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-21 09:21:46 -0700

    update

--- stock_picking_projects/models/product_product.py
+++ stock_picking_projects/models/product_product.py
@@ -30,13 +30,10 @@ class ProductProduct(models.Model):
             tipo_cambio = record.project_id.exchange_rate
 
             if record.currency_id.name == 'USD' and record.project_id.exchange_rate > 0:
-                record.supplier_cost = self.pesos_a_dolares(monto,tipo_cambio)
-            elif record.currency_id.name == 'USD'
-                record.supplier_cost = monto
-
-            if record.currency_id.name == 'MXN' and record.project_id.exchange_rate > 0:
+                record.supplier_cost = self.pesos_a_dolares(monto,tipo_cambio)    
+            elif record.currency_id.name == 'MXN' and record.project_id.exchange_rate > 0:
                 record.supplier_cost = self.dolares_a_pesos(monto,tipo_cambio)
-            elif 
+            else 
                 record.supplier_cost = monto
             
     @api.depends('quantity','product_id','project_id.exchange_rate','project_id.currency_id')
