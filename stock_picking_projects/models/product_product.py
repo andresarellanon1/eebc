0952b92043b01066d26ad35ffbfe2ef3b1e3a8d4
JesusRamon1 <125341768+JesusRamon1@users.noreply.github.com>
2024-10-22 12:43:25 -0700

    update

--- stock_picking_projects/models/product_product.py
+++ stock_picking_projects/models/product_product.py
@@ -1,8 +1,6 @@
 from odoo import models, fields, api
 import logging
 
-_logger = logging.getLogger(__name__)
-
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
@@ -41,7 +39,7 @@ class ProductProduct(models.Model):
 
             if project_currency == 'USD' and record.project_id.exchange_rate > 0:
                 if origin_currency == 'MXN' or record.cambio == True :
-                    record.supplier_cost = self.pesos_a_dolares(monto,tipo_cambio)
+                    record.supplier_cost = str(self.pesos_a_dolares(monto,tipo_cambio)) + ' ' + 'MXN'
                     record.currency = 'USD'
 
                     if origin_currency == 'USD':
@@ -49,11 +47,10 @@ class ProductProduct(models.Model):
                     else:
                         record.cambio = True
                 else:
-                    record.supplier_cost = monto
-
+                    record.supplier_cost = str(monto) + ' ' + origin_currency
             elif project_currency == 'MXN' and record.project_id.exchange_rate > 0:
                 if origin_currency == 'USD' or record.cambio == True :
-                    record.supplier_cost = self.dolares_a_pesos(monto,tipo_cambio)
+                    record.supplier_cost = str(self.dolares_a_pesos(monto,tipo_cambio)) + ' ' + 'USD'
                     record.currency = 'MXN'
 
                     if origin_currency == 'MXN':
@@ -61,18 +58,24 @@ class ProductProduct(models.Model):
                     else:
                         record.cambio = True
                 else:
-                    record.supplier_cost = monto
+                    record.supplier_cost = str(monto) + ' ' + origin_currency
             else :
-                record.supplier_cost = monto
+                record.supplier_cost = str(monto) + ' ' + origin_currency
 
             
     @api.depends('quantity','product_id','project_id.exchange_rate','project_id.currency_id')
     def _compute_total_cost(self):
         for record in self:
+            origin_currency = record.product_id.product_tmpl_id.last_supplier_last_order_currency_id.name
             total = (record.supplier_cost * record.quantity)
             impuestos = ((total) * record.product_id.product_tmpl_id.taxes_id.amount)/100
 
-            record.total_cost = total + impuestos
+            if origin_currency == 'MXN' and record.cambio == True :
+                record.total_cost = str(total + impuestos) + ' ' + 'USD'
+            elif origin_currency == 'USD' and record.cambio == True :
+                record.total_cost = str(total + impuestos) + ' ' + 'MXN'
+            else:
+                record.total_cost = str(total + impuestos) + ' ' + origin_currency
 
     def pesos_a_dolares(self, monto, tipo_cambio):
         return monto / tipo_cambio
