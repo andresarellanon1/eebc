3cc7abe1ed98115614bcf5410a340770442568f4
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-25 11:04:42 -0700

    update

--- project_plan_history/models/project_picking_line.py
+++ project_plan_history/models/project_picking_line.py
@@ -3,4 +3,4 @@ from odoo import fields, models, api
 class ProjectProject(models.Model):
     _inherit = 'project.picking.lines'
 
-    version_id = fields.Many2one('project.version', string="History")
+    version_id = fields.Many2one('project.version', string="History ")
