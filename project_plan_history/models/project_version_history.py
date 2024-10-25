9caddfd11d5aaef369a6f1684c80b66104571db7
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-25 11:35:50 -0700

    update

--- project_plan_history/models/project_version_history.py
+++ project_plan_history/models/project_version_history.py
@@ -16,10 +16,9 @@ class ProjectVersion(models.Model):
     description = fields.Text(string='Description')
     date_start = fields.Date(string='Start date')
 
-    project_plan_lines = fields.One2many('project.plan.line', 'version_id', string='Planeación')
-    project_picking_lines = fields.One2many('project.picking.lines', 'version_id', string='Stock')
+    project_plan_lines = fields.One2many('project.project', 'version_id', string='Planeación')
+    project_picking_lines = fields.One2many('project.project', 'version_id', string='Stock')
     # project_ids = fields.One2many('project.project', 'version_id', string='Historial')
-    
     @api.model
     def create_version(self, project, user):
         # Guardamos los datos del proyecto
@@ -31,7 +30,7 @@ class ProjectVersion(models.Model):
             'project_name': project.name,
             'description': project.description,
             'date_start': project.date_start,
-            'project_plan_lines': [(0, 0, {
+            'project_ids': [(0, 0, {
                 'name': line.name,
             }) for line in project.project_plan_lines],
         })
