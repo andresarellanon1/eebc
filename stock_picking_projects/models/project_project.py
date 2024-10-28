6983e5c12ead0015fcf54283ecec0ca3d2710438
Ramon-hash707 <ramon@quadrosoluciones.mx>
2024-10-28 12:28:20 -0700

    update

--- stock_picking_projects/models/project_project.py
+++ stock_picking_projects/models/project_project.py
@@ -115,26 +115,11 @@ class ProjectProject(models.Model):
     @api.depends('product_ids.quantity')
     def _total_final_cost(self):
         for record in self:
-            record.costo_total_final = 0 
-            for product in record.product_ids:
-                total = (product.supplier_cost * product.quantity)
-                impuestos = ((total) * record.taxes_id.amount)/100
-                origin_currency = product.product_id.product_tmpl_id.last_supplier_last_order_currency_id.name
-                _logger.warning('Entro al depends')
-                if product.supplier_cost > 0:
-                    costo_total = total + impuestos
-                    record.costo_total_final =  record.costo_total_final + costo_total
+            _logger.warning(f'El nuevo valor de costo prueba:')
+            _logger.warning(f'El nuevo valor de costo prueba dos:')
+            record.costo_total_final = record.product_ids.quantity * 100
+            _logger.warning(f'El nuevo valor de costo_total_final:')
 
-                    if origin_currency == 'USD' or origin_currency == 'MXN':
-                        if origin_currency == 'MXN' and product.cambio == True :
-                            record.display_costo_total_final = f"{record.costo_total_final:.2f} USD"
-                            _logger.warning(f'Valor de costo total: {display_costo_total_final}')
-                        elif origin_currency == 'USD' and product.cambio == True :
-                            record.display_costo_total_final = f"{record.costo_total_final:.2f} MXN"
-                            _logger.warning(f'Valor de costo total: {display_costo_total_final}')
-                        else:
-                            record.display_costo_total_final = f"{record.costo_total_final:.2f} {origin_currency}"
-                            _logger.warning(f'Valor de costo total: {display_costo_total_final}')
             
             
             
