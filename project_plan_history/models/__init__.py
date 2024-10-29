ea19493ae4585b6658f89a5ec11373b72d2846ff
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-10-29 11:01:21 -0700

    update

--- project_plan_history/models/__init__.py
+++ project_plan_history/models/__init__.py
@@ -2,4 +2,4 @@ from . import project_version_history
 from . import project_project
 from . import project_picking_line
 from . import project_plan_line
-# from . import change_reason_wizard
+from . import change_reason_wizard
