3316dbf0512725a46dba99c0c6f1c070b37e2fd0
YaedQuadro <161377150+ElYaedH@users.noreply.github.com>
2024-10-31 15:04:30 -0700

    Update project_project.py

--- projects_modifications_history/models/project_project.py
+++ projects_modifications_history/models/project_project.py
@@ -3,22 +3,24 @@ from odoo import models, api, fields
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    @api.multi
     def write(self, vals):
-        wizard = self.env['project.version.wizard'].create({
-            'modification_date': fields.Datetime.now(),
-            'modified_by': self.env.user.id,
-            'project_plan_lines': [(6, 0, [line.id for line in self.project_plan_lines])],
-            'project_picking_lines': [(6, 0, [line.id for line in self.project_picking_lines])],
-        })
+        for record in self:
+            wizard = self.env['project.version.wizard'].create({
+                'modification_date': fields.Datetime.now(),
+                'modified_by': self.env.user.id,
+                'project_plan_lines': [(6, 0, [line.id for line in record.project_plan_lines])],
+                'project_picking_lines': [(6, 0, [line.id for line in record.project_picking_lines])],
+            })
 
-        return {
-            'name': 'Project Version History',
-            'view_mode': 'form',
-            'res_model': 'project.version.wizard',
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-            'context': {
-                'default_project_id': self.id
+            return {
+                'name': 'Project Version History',
+                'view_mode': 'form',
+                'res_model': 'project.version.wizard',
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'context': {
+                    'default_project_id': record.id
+                }
             }
-        }
+
+        return super(ProjectProject, self).write(vals)
