3394d4e9347e02c86b4359c8599788f629ba0a55
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-10-31 10:01:04 -0700

    add task creation button

--- project_plan_templates/models/project_project.py
+++ project_plan_templates/models/project_project.py
@@ -1,13 +1,23 @@
 from odoo import fields, api, models
 from odoo.exceptions import ValidationError
 
-class ProjectProject(models.Model):
 
+class ProjectProject(models.Model):
     _inherit = 'project.project'
-    
+
     project_plan_id = fields.Many2one('project.plan', string="Project template", readonly="True")
     project_plan_lines = fields.One2many('project.plan.line', 'origin_project_id', string="Project plan lines")
     project_picking_ids = fields.Many2many('project.plan.pickings', string="Stock picking")
     project_picking_lines = fields.One2many('project.picking.lines', 'project_id', string="Project picking lines")
 
-    
+    def action_create_task(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Nueva Tarea',
+            'view_mode': 'form',
+            'res_model': 'project.task',
+            'target': 'new',
+            'context': {
+                'default_project_id': self.id,
+            }
+        }
