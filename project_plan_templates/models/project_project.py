5fba67d89dcfaf78e280de17e71d1930a702453b
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-10-31 12:45:07 -0700

    undo today commits

--- project_plan_templates/models/project_project.py
+++ project_plan_templates/models/project_project.py
@@ -10,14 +10,3 @@ class ProjectProject(models.Model):
     project_picking_ids = fields.Many2many('project.plan.pickings', string="Stock picking")
     project_picking_lines = fields.One2many('project.picking.lines', 'project_id', string="Project picking lines")
 
-    def action_create_task(self):
-        return {
-            'type': 'ir.actions.act_window',
-            'name': 'Crear Nueva Tarea',
-            'view_mode': 'form',
-            'res_model': 'project.task.create.wizard',
-            'target': 'new',  # Abre la vista en un pop-up
-            'context': {
-                'default_project_id': self.id,  # Asigna el proyecto actual en el wizard
-            }
-        }
