e5d267f14480e896cf205320a784fc96fe9d0c00
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-11-01 11:40:05 -0700

    Update project_version_wizard.py

--- projects_modifications_history/models/project_version_wizard.py
+++ projects_modifications_history/models/project_version_wizard.py
@@ -9,13 +9,7 @@ class ProjectVersionWizard(models.TransientModel):
     modification_date = fields.Datetime(string='Modification date')
     modification_motive = fields.Html(string='Motive of adjustment')
     modified_by = fields.Many2one('res.users', string='Modified by')
-    project_plan_id = fields.Many2one('project.plan', string="Project Plan", required=True, readonly="True")
     
-    project_plan_pickings = fields.Many2many(
-        'project.plan.pickings', 
-        string="Picking Templates"
-    )
-
     project_plan_lines = fields.Many2many(
         'project.plan.line',
         string='Planeación'
@@ -30,12 +24,21 @@ class ProjectVersionWizard(models.TransientModel):
     def action_confirm_version_history(self):
         self.ensure_one()
 
-        history = self.env['project.version.history'].create({
-            'project_id': self.project_id.id,
-            'modified_by': self.modified_by.id,
-            'modification_motive': self.modification_motive,
-            'project_name': self.project_id.name,
-        })
+        project = self.env['project.project'].browse(self.project_id.id)
+
+        existing_history = self.env['project.version.history'].search([
+            ('project_id', '=', self.project_id.id)
+        ], limit=1)
+
+        if not existing_history:
+            history = self.env['project.version.history'].create({
+                'project_id': self.project_id.id,
+                'modified_by': self.modified_by.id,
+                'modification_motive': self.modification_motive,
+                'project_name': self.project_id.name,
+            })
+        else:
+            history = existing_history
 
         self.env['project.version.lines'].create({
             'project_version_history_id': history.id,
@@ -46,11 +49,10 @@ class ProjectVersionWizard(models.TransientModel):
             'project_picking_lines': [(6, 0, self.project_picking_lines.ids)],
         })
 
-        self.project_id.write({
-            
+        project.write({
+
         })
 
-        
         return {
             'type': 'ir.actions.act_window_close'
         }
