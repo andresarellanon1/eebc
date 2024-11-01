362058013cbe122ca38552b971d9d0f1706e73be
enriquequadro <enrique@quadrosoluciones.mx>
2024-11-01 09:51:31 -0700

    fix wizard & update task

--- project_plan_templates/models/project_task.py
+++ project_plan_templates/models/project_task.py
@@ -3,15 +3,8 @@ from odoo import models, fields
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    chapter = fields.Char(string='Chapter')
     description = fields.Text(string='Description')
-    product_uom = fields.Many2one('uom.uom', string='Unit of Measure')
-    unit_price = fields.Float(string='Unit Price')
-    amount_total = fields.Float(string='Total Amount')
-    use_project_task = fields.Boolean(string='Use Project Task')
-    stage_id = fields.Many2one('project.task.type', string='Stage')
     planned_date_begin = fields.Datetime(string='Planned Start Date')
     planned_date_end = fields.Datetime(string='Planned End Date')
     origin_project_id = fields.Many2one('project.project', string='Origin Project')
     partner_id = fields.Many2one('res.partner', string='Partner')
-    task_timesheet_id = fields.Many2one('account.analytic.line', string='Task Timesheet')
