752ad9f64a614d6cfff1fb4a2d01fa5cb79f0dc3
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-11-01 11:42:50 -0700

    update

--- project_plan_templates/__manifest__.py
+++ project_plan_templates/__manifest__.py
@@ -20,7 +20,7 @@
         "views/timesheet_template_view.xml",
         "views/inherit_project_task_form_inherited2.xml",
         "views/plan_project_stock_tree_view.xml",
-        "views/view_task_inventory_wizard.xml",
+        "wizard/view_task_inventory_wizard.xml",
     ],
     "license": "LGPL-3",
     'installable': True,
