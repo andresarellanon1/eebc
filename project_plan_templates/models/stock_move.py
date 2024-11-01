4f220ca912a50ebdab9dda5e1aa00505bbf98e68
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-11-01 10:00:47 -0700

    update

--- project_plan_templates/models/stock_move.py
+++ project_plan_templates/models/stock_move.py
@@ -4,6 +4,19 @@ class StockMove(models.Model):
    
     _inherit = 'stock.move'
 
-    product_id = fields.Many2one('product.product', string='product', domain="[('id', 'in', 'picking_id.task_id.project_id.project_picking_lines.mapped('product_id').ids')]")
+    product_id = fields.Many2one(
+        'product.product',
+        string='Producto',
+        domain=lambda self: self._get_product_domain()
+    )
+
+    @api.model
+    def _get_product_domain(self):
+        # Aquí puedes obtener el contexto necesario para determinar el dominio.
+        if self.picking_id and self.picking_id.task_id and self.picking_id.task_id.project_id:
+            # Obtener los IDs de los productos de project_picking_lines
+            product_ids = self.picking_id.task_id.project_id.project_picking_lines.mapped('product_id.id')
+            return [('id', 'in', product_ids)]
+        return []
 
     # stock.move (picking_id) - stock.picking (task_id) - project.task (project_id) - project.project (project_picking_lines) project.project (project_picking_ids)
