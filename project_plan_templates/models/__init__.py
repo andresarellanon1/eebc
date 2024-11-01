07ff103b96315384b6ce3b2b1566eb8b5b4fc21f
enriquequadro <enrique@quadrosoluciones.mx>
2024-11-01 10:21:42 -0700

    rm task model

--- project_plan_templates/models/__init__.py
+++ project_plan_templates/models/__init__.py
@@ -6,4 +6,3 @@ from . import project_creation_wizard
 from . import task_timesheet
 from . import task_time_lines
 from . import account_analytic_line
-from . import project_task
--- project_plan_templates/models/project_task.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ProjectTask(models.Model):
-    _inherit = 'project.task'
-
-    description = fields.Text(string='Description')
-    planned_date_begin = fields.Datetime(string='Planned Start Date')
-    planned_date_end = fields.Datetime(string='Planned End Date')
-    partner_id = fields.Many2one('res.partner', string='Partner')
