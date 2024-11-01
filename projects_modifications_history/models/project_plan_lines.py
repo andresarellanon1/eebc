a87a34ab9e56bce911aa8ead48f2bf65ac98f230
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-11-01 09:02:50 -0700

    Update project_plan_lines.py

--- projects_modifications_history/models/project_plan_lines.py
+++ projects_modifications_history/models/project_plan_lines.py
@@ -2,6 +2,6 @@ from odoo import fields, models, api
 
 class ProjectPlanLines(models.Model):
 
-    _inherit = 'project.plan.lines'
+    _inherit = 'project.plan.line'
 
     project_version_id = fields.Many2one('project_version_history', string="Project Version")
