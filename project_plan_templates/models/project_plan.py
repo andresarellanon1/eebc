09dd96d539599b4e2008827e680aac2ac9bd7f07
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-11-04 13:09:32 -0800

    project_plan_templates:comments

--- project_plan_templates/models/project_plan.py
+++ project_plan_templates/models/project_plan.py
@@ -18,6 +18,12 @@ class ProjectPlan(models.Model):
         string="Picking Lines"
     )
 
+    # This method allows the user to select multiple inventory templates 
+    # and combines all their products into a single list. 
+    # When the 'project_plan_pickings' field is modified, 
+    # it aggregates the 'project_picking_lines' from each selected picking 
+    # and assigns the combined list to 'picking_lines' in the current record.
+
     @api.onchange('project_plan_pickings')
     def onchange_picking_lines(self):
         for record in self:
@@ -25,6 +31,16 @@ class ProjectPlan(models.Model):
             for picking in record.project_plan_pickings:
                 lines |= picking.project_picking_lines
             record.picking_lines = lines
+
+    # This action opens a wizard to create a project from the current template.
+    # It allows the user to generate the project's tasks, timesheets, and inventory.
+    # The wizard is displayed in a form view and opens as a modal dialog.
+    # The context now includes the following default values for the wizard:
+    # - 'default_project_plan_id': the current template's ID
+    # - 'default_project_plan_lines': the IDs of the project plan lines
+    # - 'default_project_plan_pickings': the IDs of the inventory templates
+    # - 'default_picking_lines': the IDs of the picking lines
+    # - 'default_description': the current template's description
 
     def action_open_create_project_wizard(self):
         self.ensure_one()
@@ -35,6 +51,10 @@ class ProjectPlan(models.Model):
             'type': 'ir.actions.act_window',
             'target': 'new',
             'context': {
-                'default_project_plan_id': self.id
+                'default_project_plan_id': self.id,
+                'default_project_plan_lines': [(6, 0, self.project_plan_lines.ids)],
+                'default_project_plan_pickings': [(6, 0, self.project_plan_pickings.ids)],
+                'deafult_picking_lines': [(6, 0, self.picking_lines.ids)],
+                'default_description': self.description,
             }
         }
