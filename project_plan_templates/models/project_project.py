4ce4018da2154ac8684eaa1d622891e46bb7c85c
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-11-05 12:48:37 -0800

    undo

--- project_plan_templates/models/project_project.py
+++ project_plan_templates/models/project_project.py
@@ -1,44 +1,63 @@
-from odoo import models, api, fields
+from odoo import fields, api, models
+from odoo.exceptions import UserError
 
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
+    project_plan_id = fields.Many2one('project.plan', string="Project template", readonly="True")
+    project_plan_lines = fields.One2many('project.plan.line', 'origin_project_id', string="Project plan lines")
+    project_picking_ids = fields.Many2many('project.plan.pickings', string="Stock picking")
+    project_picking_lines = fields.One2many('project.picking.lines', 'project_id', string="Project picking lines")
 
-    version_history_ids = fields.One2many(
-        'project.version.history',
-        'project_id',
-        string='Historial de modificaciones',
-        readonly=True,
-    )
-
-    version_history_id = fields.Many2one(
-        'project.version.history',
-        string='Version History',
-        compute='_compute_version_history_id',
-        store=True
-    )
-
-    @api.depends('version_history_ids')
-    def _compute_version_history_id(self):
+    ### Crea tareas de proyecto a partir de las líneas del plan de proyecto.
+    # Valida si la tarea ya existe, si existe alguna, cancela el proceso y notifica al usuario.
+    def create_project_tasks(self):
         for project in self:
-            version_history = self.env['project.version.history'].search([('project_id', '=', project.id)], limit=1)
-            project.version_history_id = version_history if version_history else False
+            for line in project.project_plan_lines:
+                current_task_type = self.get_or_create_task_type(line.stage_id or 'Extras', project)
 
-    def action_save_version(self):
-        self.ensure_one()
+                if line.use_project_task:
+                    # Verificar si la tarea ya existe
+                    existing_task = self.env['project.task'].search([
+                        ('name', '=', line.name),
+                        ('project_id', '=', project.id)
+                    ], limit=1)
 
-        return {
-            'name': 'Project Version History',
-            'view_mode': 'form',
-            'res_model': 'project.version.wizard',
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-            'context': {
-                'default_project_id': self.id,
-                'default_project_plan_id': self.project_plan_id.id,
-                'default_project_plan_lines': [(6, 0, self.project_plan_lines.ids)],
-                'default_project_picking_lines': [(6, 0, self.project_picking_lines.ids)],
-                'default_modified_by': self.env.user.id,
-                'default_modification_date': fields.Datetime.now(),
-            }
-        }
+                    if not existing_task:
+                        # Crear la nueva tarea si no existe
+                        timesheet_lines = self.env['task.time.lines'].search([
+                            ('task_timesheet_id', '=', line.task_timesheet_id.id)
+                        ])
+
+                        timesheet_data = [(0, 0, {
+                            'name': ts_line.description,
+                            'estimated_time': ts_line.estimated_time,
+                        }) for ts_line in timesheet_lines]
+
+                        self.env['project.task'].create({
+                            'name': line.name,
+                            'project_id': project.id,
+                            'description': line.description,
+                            'planned_date_begin': line.planned_date_begin,
+                            'date_deadline': line.planned_date_end,
+                            'user_ids': [(6, 0, line.partner_id.ids)],
+                            'stage_id': current_task_type.id,
+                            'timesheet_ids': timesheet_data,
+                        })
+
+    ### Busca o crea un tipo de tarea por nombre y proyecto.
+    # Args: stage_id: Nombre del tipo de tarea; project: Proyecto relacionado.
+    # Returns: Tipo de tarea encontrado o creado.
+    def get_or_create_task_type(self, stage_id, project):
+        task_type = self.env['project.task.type'].search([
+            ('name', '=', stage_id),
+            ('project_ids', 'in', project.id)
+        ], limit=1)
+
+        if not task_type:
+            task_type = self.env['project.task.type'].create({
+                'name': stage_id,
+                'project_ids': [(4, project.id)],
+            })
+
+        return task_type
