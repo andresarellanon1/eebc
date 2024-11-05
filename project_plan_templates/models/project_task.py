900009b1e6844f7527ff481344c378ab78249d67
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-11-05 11:43:57 -0800

    update

--- project_plan_templates/models/project_task.py
+++ project_plan_templates/models/project_task.py
@@ -24,17 +24,17 @@ class ProjectTask(models.Model):
 
 # stock.move (picking_id) - stock.picking (task_id) - project.task (project_id) - project.project (project_picking_lines) project.project (project_picking_ids)
 
-    # def action_create_inventory(self):
-    #     inventory_vals = {
-    #         'origin': self.name,
-    #         'picking_type_id': self.project_id.default_picking_type_id.id,
-    #         'task_id': self.id,
-    #     }
-    #     inventory = self.env['stock.picking'].create(inventory_vals)
-    #     return {
-    #         'type': 'ir.actions.act_window',
-    #         'res_model': 'stock.picking',
-    #         'res_id': inventory.id,
-    #         'view_mode': 'form',
-    #         'target': 'new',
-    #     }
+    def action_create_inventory(self):
+        inventory_vals = {
+            'origin': self.name,
+            'picking_type_id': self.project_id.default_picking_type_id.id,
+            'task_id': self.id,
+        }
+        inventory = self.env['stock.picking'].create(inventory_vals)
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'stock.picking',
+            'res_id': inventory.id,
+            'view_mode': 'form',
+            'target': 'new',
+        }
