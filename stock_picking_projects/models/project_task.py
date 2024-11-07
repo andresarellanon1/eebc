d4dffe0970cea268cdc23f56ead49170b5f89fca
EMZ1020 <emiliano@quadrosoluciones.mx>
2024-11-07 11:48:33 -0800

    Update project_task.py

--- stock_picking_projects/models/project_task.py
+++ stock_picking_projects/models/project_task.py
@@ -1,22 +1,19 @@
-from odoo import models, fields, api
+from odoo import api, fields, models
 
-class ProjectTask(models.Model):
-
+class ProjectTask(models.Model): 
     _inherit = 'project.task'
 
-    stock_ids = fields.One2many('stock.picking', 'task_id', string="stock")
+    stock_ids = fields.One2many('stock.move', 'task_id', string="Stock Moves", compute="_compute_stock_ids", store=True)
 
     @api.depends('project_id.default_picking_type_id')
     def _compute_stock_ids(self):
         for task in self:
             if task.project_id.default_picking_type_id:
                 picking_type_id = task.project_id.default_picking_type_id.id
-                
                 stock_moves = self.env['stock.move'].search([
                     ('picking_type_id', '=', picking_type_id),
                     ('project_id', '=', task.project_id.id)
                 ])
-                
                 task.stock_ids = stock_moves
             else:
                 task.stock_ids = False
