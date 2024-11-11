f98611a0630cb2c790ad921a674e14340787fbda
enriquequadro <enrique@quadrosoluciones.mx>
2024-11-11 11:28:24 -0800

    product_product

--- project_plan_templates/models/__init__.py
+++ project_plan_templates/models/__init__.py
@@ -11,5 +11,6 @@ from . import project_task
 from . import stock_picking
 from . import sale_order
 from . import sale_order_line
+from . import product_product
 #from . import product_template
 #from . import stock_move
