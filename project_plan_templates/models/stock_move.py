37fdfa7221ce7ebcb4be2aabe470fc08c9e6ab47
enriquequadro <enrique@quadrosoluciones.mx>
2024-11-11 13:05:28 -0800

    fix

--- project_plan_templates/models/stock_move.py
+++ project_plan_templates/models/stock_move.py
@@ -4,10 +4,5 @@ class StockMove(models.Model):
 
     _inherit = 'stock.move'
 
-    product_id = fields.Many2one(
-        'product.product', 'Product',
-        check_company=True,
-        domain="[('type', '=', 'consu')]", index=True, required=True)
-
     product_uom = fields.Many2one(
         'uom.uom', string="Unidad", domain="[('category_id', '=', product_uom_category_id)]")
