24eb9105691f69ff480071820aad9304c8a2d3f7
enriquequadro <enrique@quadrosoluciones.mx>
2024-11-11 11:33:59 -0800

    fixx

--- project_plan_templates/models/__init__.py
+++ project_plan_templates/models/__init__.py
@@ -10,7 +10,6 @@ from . import task_inventory_wizard
 from . import project_task
 from . import stock_picking
 from . import sale_order
-from . import sale_order_line
 from . import product_product
 from . import project_sale_creation_wizard
 from . import product_template
