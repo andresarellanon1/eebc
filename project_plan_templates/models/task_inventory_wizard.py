9351c5c489fc69a09c1a3ba33afdd9e5b8593f23
notyisuschrist <jesus.gutierrez50@uabc.edu.mx>
2024-11-12 13:49:16 -0800

    update

--- project_plan_templates/models/task_inventory_wizard.py
+++ project_plan_templates/models/task_inventory_wizard.py
@@ -9,46 +9,30 @@ class ProjectCreation(models.TransientModel):
     _description = 'Wizard to confirm project creation'
 
     project_task_id = fields.Many2one('project.task', string="Project Task")
-
     stock_move_ids = fields.Many2many('stock.move', string="Stock move")
-    product_ids = fields.Many2many('product.product', string="Productos",
-                                   domain=lambda self: self._get_product_domain())
+    product_ids = fields.Many2many('product.product', string="Productos")
     stock_picking_ids = fields.Many2many('stock.picking', string="Stock picking")
-
-    # stock_move_id = fields.Many2many('stock.move', string="Stock move" )
 
     name = fields.Char(string='Referencia')
     partner_id = fields.Many2one('res.partner', string='Contacto')
-    picking_type_id = fields.Many2one('stock.picking.type', string='Tipo de operación',
-                                      compute='_compute_picking_type_id', store=True)
+    picking_type_id = fields.Many2one('stock.picking.type', string='Tipo de operación', compute='_compute_picking_type_id', store=True)
     location_id = fields.Many2one('stock.location', string='Ubicación de origen')
     location_dest_id = fields.Many2one('stock.location', string='Ubicación de destino')
     scheduled_date = fields.Datetime(string='Fecha programada')
     origin = fields.Char(string='Documento origen', compute="_compute_origin", store=True)
-
     task_id = fields.Many2one('stock.picking', string='Tarea de origen')
     task_id_char = fields.Char(string='Tarea origen', compute="_compute_task_id")
-
     user_id = fields.Many2one('res.users', string='Contacto')
+    product_packaging_id = fields.Many2one('product.packaging', 'Packaging', domain="[('product_id', '=', product_id)]", check_company=True)
 
-    product_packaging_id = fields.Many2one('product.packaging', 'Packaging', domain="[('product_id', '=', product_id)]",
-                                           check_company=True)
-
-    # Sección de Información adicional
-
+    # Información adicional
     carrier_id = fields.Many2one('delivery.carrier')
     carrier_tracking_ref = fields.Char(string="Referencia de rastreo")
     weight = fields.Float(string="Peso")
     shipping_weight = fields.Float(string="Peso para envío")
-
     group_id = fields.Many2one('procurement.group', string="Grupo de aprovisionamiento")
     company_id = fields.Many2one('res.company', string="Empresa")
-
-    transport_type = fields.Selection(string="Tipo de transporte",
-                                      selection=[('00', 'No usa carreteras federales'),
-                                                 ('01', 'Autotransporte Federal')])
-    # customs regimes
-    # customs document type
+    transport_type = fields.Selection(string="Tipo de transporte",selection=[('00', 'No usa carreteras federales'), ('01', 'Autotransporte Federal')])
     custom_document_identification = fields.Char(string="Customs Document Identification")
 
     lat_origin = fields.Float(string="Latitud de origen")
@@ -56,17 +40,21 @@ class ProjectCreation(models.TransientModel):
     lat_dest = fields.Float(string="Latitud de destino")
     long_dest = fields.Float(string="Longitud de destino")
 
+    # Dominio para los productos
+    project_picking_product_ids = fields.Many2many(
+        'product.product',
+        compute='_compute_project_picking_product_ids',
+        string="Productos de Picking del Proyecto"
+    )
+
+    @api.depends('project_picking_lines')
+    def _compute_project_picking_product_ids(self):
+        for record in self:
+            record.project_picking_product_ids = record.project_picking_lines.mapped('product_ids')
+
     @api.onchange('name')
     def _compute_task_id(self):
         self.task_id_char = self.project_task_id.name
-
-    # @api.onchange('name')
-    # def _compute_task_id(self):
-    #     for record in self:
-    #         if record.name:
-    #             task = self.env['project.task'].search([('id', '=', record.name)], limit=1)
-    #             if task:
-    #                 record.task_id = task.id
 
     @api.onchange('name')
     def _compute_picking_type_id(self):
@@ -77,20 +65,6 @@ class ProjectCreation(models.TransientModel):
     def _compute_origin(self):
         _logger.warning(f'El valor de origin es: {self.project_task_id.name}')
         self.origin = self.project_task_id.name
-
-    # def _get_product_domain(self):
-    #     # Calculamos el dominio para filtrar solo los productos que ya están asociados a las líneas de picking del picking
-    #     picking_ids = self.stock_picking_ids.ids  # Obtenemos los IDs de los picking seleccionados
-    #     if not picking_ids:
-    #         return [('id', '=', False)]  # Si no hay picking seleccionado, no permitimos seleccionar productos
-
-    #     # Recuperamos los productos asociados a las líneas de project.picking.lines
-    #     products_in_picking_lines = self.env['project.picking.line'].search([
-    #         ('picking_id', 'in', picking_ids)
-    #     ]).mapped('product_id')  # Obtener los productos asociados
-
-    #     # El dominio es de los productos que están asociados a esas líneas de picking
-    #     return [('id', 'in', products_in_picking_lines.ids)]
 
     def action_confirm_create_inventory(self):
         self.ensure_one()
@@ -134,10 +108,6 @@ class ProjectCreation(models.TransientModel):
             'long_dest': self.long_dest,
         }
 
-        stock_picking = self.env['stock.picking'].create(stock_picking_vals)
-
-        # Asignar el picking a la tarea
-        if self.project_id:
-            self.project_task_id.stock_ids = [(4, stock_picking.id)]
+        self.env['stock.picking'].create(stock_picking_vals)
 
         return True
