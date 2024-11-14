9c678e906e1309c3a1c7ae55f3ca52ca4e58c7b2
AntonioGarcia324 <antonio@quadrosoluciones.mx>
2024-11-14 11:04:11 -0800

    update

--- projects_modifications_history/models/project_version_lines.py
+++ projects_modifications_history/models/project_version_lines.py
@@ -95,6 +95,27 @@ class ProjecVersionLines(models.Model):
                     'location_id': line.location_id.id  # Cambia esto a los campos relevantes
                 }) for line in previous_version.project_picking_lines]
                 record.previous_version_picking_lines = new_picking_lines
+
+                # Duplicar plan lines y picking lines de la versión anterior
+                new_actual_plan_lines = [(0, 0, {
+                    'name': line.name,  # Copiar atributos relevantes
+                    'chapter': line.chapter,
+                    'description': line.description,
+                    'planned_date_begin': line.planned_date_begin, 
+                    'planned_date_end': line.planned_date_end, 
+                    'partner_id': line.partner_id,
+                    'task_timesheet_id': line.task_timesheet_id,
+                    'stage_id': line.stage_id, # Cambia esto a los campos relevantes
+                }) for line in record.project_plan_lines]
+                record.project_plan_lines = new_actual_plan_lines
+    
+                new_actual_picking_lines = [(0, 0, {
+                    'product_id': line.product_id.id,
+                    'quantity': line.quantity,
+                    'reservado': line.reservado,
+                    'location_id': line.location_id.id  # Cambia esto a los campos relevantes
+                }) for line in record.project_picking_lines]
+                record.project_picking_lines = new_actual_picking_lines
     
                 # if previous_version:
                 #     record.previous_version_plan_lines = previous_version.project_plan_lines
@@ -103,17 +124,11 @@ class ProjecVersionLines(models.Model):
                 #     record.previous_version_picking_lines = previous_version.project_picking_lines
                 #     _logger.warning(f'El record cambio es: {previous_version.project_picking_lines}')
 
-                # Actualizar `project_plan_lines` y `project_picking_lines` agregando las nuevas líneas
-                record.write({
-                    'project_plan_lines': [(4, line.id) for line in record.project_plan_lines] + new_plan_lines,
-                    'project_picking_lines': [(4, line.id) for line in record.project_picking_lines] + new_picking_lines,
-                })
-
                 # record.project_plan_lines = record.project_plan_lines | previous_version.project_plan_lines
-                _logger.warning(f'El record cambio es: {record.project_plan_lines}')
+                # _logger.warning(f'El record cambio es: {record.project_plan_lines}')
 
                 # record.project_picking_lines = record.project_picking_lines | previous_version.project_picking_lines
-                _logger.warning(f'El record cambio es: {record.project_picking_lines}')
+                # _logger.warning(f'El record cambio es: {record.project_picking_lines}')
 
                 record.has_previous_version = True
             else:
