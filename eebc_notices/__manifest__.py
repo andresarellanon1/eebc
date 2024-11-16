0e18e4066711094fdb10a9eb00970a920140ed93
alejandro ramos <ivan.alj@hotmail.com>
2024-11-15 16:09:55 -0800

    test nuevo wizard de seleccion v15

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.69',
+    'version': '17.0.1.70',
     'depends': ["stock"],
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
