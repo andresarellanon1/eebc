8225f5c507896c7c3ac039345c33da2e8de96eaf
alejandro ramos <ivan.alj@hotmail.com>
2024-11-20 12:41:21 -0800

    muchos cambios v2

--- eebc_notices/wizard/wizard_selection_line.py
+++ eebc_notices/wizard/wizard_selection_line.py
@@ -7,10 +7,10 @@ class WizardSelectionLine(models.TransientModel):
     _description = 'Wizard Selection Line'
 
     wizard_id = fields.Many2one('select.notice.wizard', string='Wizard', required=True)
-    notice_history_ids = fields.Many2many('notices.history', string='Aviso', required=True)
-    notice_ids = fields.Many2many('notices.notices', string='Aviso', required=True)
+    notice_history_ids = fields.Many2many('notices.history', required=True)
+    notice_ids = fields.Many2many('notices.notices', string='Avisos', required=True)
 
-    quantity = fields.Float(string='Quantity', default=1.0, required=True)
+    quantity = fields.Float(string='Cantidad asignada', default=0, required=True)
 
 
   
