f05626c3b82522371277cdeb53bd60953e32e905
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-11-20 14:49:43 -0800

    wwwww

--- project_plan_templates/models/project_plan.py
+++ project_plan_templates/models/project_plan.py
@@ -56,6 +56,7 @@ class ProjectPlan(models.Model):
             'target': 'new',
             'context': {
                 'default_project_plan_id': self.id,
+                'default_wizard_plan_lines': [(6, 0, self.project_plan_lines.ids)],
                 'default_project_plan_pickings': [(6, 0, self.project_plan_pickings.ids)],
                 'default_picking_lines': [(6, 0, self.picking_lines.ids)],
                 'default_description': self.description,
