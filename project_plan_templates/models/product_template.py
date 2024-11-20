b81d0aaf2c759f6c66b0de545ce93d28924e2c9d
Emiliano1020 <emiliano@quadrosoluciones.mx>
2024-11-20 11:13:01 -0800

    Update product_template.py

--- project_plan_templates/models/product_template.py
+++ project_plan_templates/models/product_template.py
@@ -6,3 +6,13 @@ class ProductTemplate(models.Model):
 
     project_plan_id = fields.Many2one('project.plan', string="Project plan template")
     service_policy = fields.Many2one('product_template', string="Service Policy")
+    service_tracking = fields.Selection(
+        selection=[
+            ('no', 'Nada'),
+            ('task_global_project', 'Tarea'),
+            ('task_in_project', 'Proyecto y tarea'),
+            ('project_only', 'Proyecto'),
+        ],
+        string='Service Tracking',
+        default='no', 
+    )
