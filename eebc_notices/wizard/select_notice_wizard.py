29edac3446d6ef8f364cb34222a7063f14127e9f
Aybancho <ivan.rosas@uabc.edu.mx>
2024-11-21 13:11:00 -0800

    test get_sale_ordes v7

--- eebc_notices/wizard/select_notice_wizard.py
+++ eebc_notices/wizard/select_notice_wizard.py
@@ -27,6 +27,8 @@ class SelectNoticeWizard(models.TransientModel):
         res = super(SelectNoticeWizard, self).default_get(fields)
         if 'stock_move_id' in self._context:
             res['stock_move_id'] = self._context['stock_move_id']
+        if 'cantidad' in self._context:
+            res['quantity'] = self._context['cantidad']
         # Asignar el mensaje de error desde el contexto
         _logger.warning('VALOR DE RES1: %s', res)
         return res
@@ -51,7 +53,9 @@ class SelectNoticeWizard(models.TransientModel):
                 ('product_id', '=', wizard.stock_move_id.product_id.id),
                 ('location_id', '=', wizard.stock_move_id.location_id.id)
             ])
+            _logger.warning('lineas de historial de aviso: %s',notice_history_ids )
             notice_ids = self.env['notices.notices'].search([('id', 'in', notice_history_ids.ids)])
+            _logger.warning('lineas de aviso: %s',notice_ids )
 
             # Limpiar las líneas existentes en caso de que haya alguna
             wizard.line_ids = [(5, 0, 0)]  # Eliminar líneas previas si existían
@@ -68,6 +72,9 @@ class SelectNoticeWizard(models.TransientModel):
             
             # Asignar las líneas al campo One2many
             wizard.line_ids = lines
+            
+            _logger.warning('lineas de wizard: %s',wizard.line_ids )
+            
 
     def action_get_products(self):
         for wizard in self:
