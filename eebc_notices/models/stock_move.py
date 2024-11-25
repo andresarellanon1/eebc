be4af3777bbf53c2abe104a3aab190d4e20ded84
alejandro ramos <ivan.alj@hotmail.com>
2024-11-25 09:43:22 -0800

    test res v4

--- eebc_notices/models/stock_move.py
+++ eebc_notices/models/stock_move.py
@@ -104,6 +104,7 @@ class StockMove(models.Model):
 
     def _create_line_ids(self):
         for move in self:
+            _logger.warning('id del producto :%s',move.product_id.id)
             if not move.id:
                 continue  # No asignar nada si no hay stock_move_id
 
