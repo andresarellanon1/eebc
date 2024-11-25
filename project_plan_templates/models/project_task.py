ad95ec4652b942531e67d628fa799c0d65413860
enriquequadro <enrique@quadrosoluciones.mx>
2024-11-25 09:47:46 -0800

    comentarios

--- project_plan_templates/models/project_task.py
+++ project_plan_templates/models/project_task.py
@@ -1,15 +1,23 @@
 from odoo import fields, models, api
 
 class ProjectTask(models.Model):
-   
     _inherit = 'project.task'
-    
-    stock_ids = fields.One2many('stock.picking', 'task_id', string="stock")
 
+    stock_ids = fields.One2many(
+        'stock.picking',
+        'task_id',
+        string="stock"
+    )
+
+    # Defining a method to open an inventory wizard
+    # (a form to create an inventory related to the task).
     def action_open_task_inventory_wizard(self):
         self.ensure_one()
+        
+        # Returns a dictionary representing an action
+        # to open a new window with a specific form view.
         return {
-            'name': 'Create inventory',
+            'name': 'Create inventory', 
             'view_mode': 'form',
             'res_model': 'task.inventory.wizard',
             'type': 'ir.actions.act_window',
