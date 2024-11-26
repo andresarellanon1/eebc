21f3c183a7e94ccdd69319fbc793fe1973d29fa6
MirkaDayanaSSS <57881931+jes-ale@users.noreply.github.com>
2024-11-25 17:29:40 -0800

    purchase_order_type: dev 4 fix

--- purchase_order_type/models/purchase_order_line.py
+++ purchase_order_type/models/purchase_order_line.py
@@ -8,8 +8,5 @@ class PurchaseOrderLine(models.Model):
 
     def _prepare_stock_move_vals(self, picking, price_unit, product_uom_qty, product_uom):
         res = super(PurchaseOrderLine, self)._prepare_stock_move_vals(picking, price_unit, product_uom_qty, product_uom)
-
-        if self.order_id and self.order_id.purchase_order_type_id:
-            res['location_dest_id'] = self.order_id.purchase_order_type_id.location_id.id
-
+        logger.warning(res)
         return res
