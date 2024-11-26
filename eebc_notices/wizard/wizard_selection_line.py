1415065fe18174f54281bbecd32be2d587281715
Aybancho <ivan.rosas@uabc.edu.mx>
2024-11-26 12:43:58 -0800

    test valuationerror quantity available v8

--- eebc_notices/wizard/wizard_selection_line.py
+++ eebc_notices/wizard/wizard_selection_line.py
@@ -8,9 +8,7 @@ class WizardSelectionLine(models.TransientModel):
     wizard_id = fields.Many2one('select.notice.wizard', string='Wizard')
     notice_id = fields.Many2one('notices.notices', string='Avisos')
     quantity = fields.Float(string='Cantidad asignada', default=0, required=True)
-    quantity_available = fields.Float(string='Cantidad disponible',
-    readonly=True 
-    )
+    quantity_available = fields.Float(string='Cantidad disponible')
 
     
     test_name = fields.Char(
