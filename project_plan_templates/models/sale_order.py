b5fc7edae3ca2dd277a8808883abe114de89bc73
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-11-27 11:10:30 -0800

    up

--- project_plan_templates/models/sale_order.py
+++ project_plan_templates/models/sale_order.py
@@ -20,6 +20,7 @@ class SaleOrder(models.Model):
     )
 
     project_plan_lines = fields.One2many('project.plan.line', 'sale_order_id')
+    project_picking_lines = fields.One2many('project.picking.lines', 'sale_order_id')
 
     def action_confirm(self):
         self.ensure_one()
@@ -27,6 +28,7 @@ class SaleOrder(models.Model):
             if sale.is_project:
                 sale.state = 'estimation'
                 plan_lines = []
+                picking_lines = []
                 for line in sale.order_line:
                     if line.product_id.project_plan_id:
                         for plan in line.product_id.project_plan_id.project_plan_lines:
@@ -40,6 +42,19 @@ class SaleOrder(models.Model):
                                     'name': f"{line.product_id.default_code}-{line.product_template_id.name}-{plan.name}",
                                     'description': plan.description,
                                     'task_timesheet_id': plan.task_timesheet_id.id,
+                                }))
+                        for picking in line.product_id.project_plan_id.picking_lines:
+                            if line.display_type == 'line_section':
+                                picking_lines.append((0, 0, {
+                                    'name': line.name,
+                                    'display_type': line.display_type
+                                }))
+                            else:
+                                picking_lines.append((0, 0, {
+                                    'product_id': picking.product_id,
+                                    'quantity': picking.quantity,
+                                    'standard_price': picking.standard_price,
+                                    'subtotal': picking.subtotal
                                 }))
                 sale.project_plan_lines = plan_lines
             else:
