38d5b7b432e648ede96a7962065e6170fd3d8a77
YaedQuadro <161377150+ElYaedH@users.noreply.github.com>
2024-11-28 15:26:42 -0800

    Update sale_order.py

--- project_plan_templates/models/sale_order.py
+++ project_plan_templates/models/sale_order.py
@@ -96,8 +96,8 @@ class SaleOrder(models.Model):
             'type': 'ir.actions.act_window',  # Action type to open a new window
             'target': 'new',  # Open in a modal ('new' window)
             'context': {
-                'default_wizard_plan_lines': [(6, 0, self.project_plan_lines)],
-                'default_wizard_picking_lines': [(6, 0, self.project_picking_lines)],
+                'default_wizard_plan_lines': [(6, 0, self.project_plan_lines.ids)],
+                'default_wizard_picking_lines': [(6, 0, self.project_picking_lines.ids)],
                 'default_sale_order_id': self.id  # Pass the current sale order ID
             }
         }
