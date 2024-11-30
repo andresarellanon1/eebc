1ecea0a746ae93776bbdb3f4f971288391c8d9d6
MirkaDayanaSSS <57881931+jes-ale@users.noreply.github.com>
2024-11-29 16:39:05 -0800

    purchase_order_type: fix

--- purchase_order_type/models/purchase_order_type.py
+++ purchase_order_type/models/purchase_order_type.py
@@ -11,9 +11,3 @@ class PurchaseOrderType(models.Model):
     name = fields.Char('Nombre', required=True)
     picking_type_id = fields.Many2one('stock.picking.type', string='Tipo de operación')
     company_id = fields.Many2one('res.company', string='Empresa')
-    is_picking_type_enabled = fields.Boolean(string='Utilizar Tipos de operación', default=True)
-
-    @api.onchange('is_picking_type_enabled')
-    def _onchange_is_picking_type_enabled(self):
-        for record in self:
-            if not record.is_picking_type_enabled: record.picking_type_id = False
