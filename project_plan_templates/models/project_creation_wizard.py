4c034a0dfbcac669278c5e76852d658b5918b246
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-12-02 11:55:26 -0800

    Update project_creation_wizard.py

--- project_plan_templates/models/project_creation_wizard.py
+++ project_plan_templates/models/project_creation_wizard.py
@@ -66,12 +66,10 @@ class ProjectCreation(models.TransientModel):
 
         return {
             'type': 'ir.actions.act_window',
-            'res_model': 'sale.order',
-            'res_id': self.sale_order_id.id,
-            'view_type': 'form',
+            'res_model': 'project.project',
+            'res_id': project.id,
             'view_mode': 'form',
             'target': 'current',
-            'context': self.env.context
         }
 
     def create_project_tasks(self, project):
