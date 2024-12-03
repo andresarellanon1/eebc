33bde98ad3ed4fd7bde6ae0db2aa46d289ed5998
Aybancho <ivan.rosas@uabc.edu.mx>
2024-12-03 12:49:03 -0800

    test js cambio pestaña v4

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.75',
+    'version': '17.0.1.76',
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
     'category': 'other',
--- eebc_notices/static/src/js/eebc_js.js
+++ eebc_notices/static/src/js/eebc_js.js
@@ -1,28 +1,31 @@
 odoo.define('eebc_notices.TabChangeHandler', [
-    'web.AbstractAction',
-    'web.core'
+    'web.FormController', // Para manejar eventos en el formulario
+    'web.core'            // Para usar utilidades y traducciones
 ], function (require) {
     'use strict';
 
-    const AbstractAction = require('web.AbstractAction');
+    const FormController = require('web.FormController');
     const core = require('web.core');
-    const _t = core._t;
 
-    AbstractAction.include({
-        events: _.extend({}, AbstractAction.prototype.events, {
-            'click .o_notebook .nav-link': '_onTabChanged', // Evento para detectar cambio de pestaña
+    FormController.include({
+        events: _.extend({}, FormController.prototype.events, {
+            'click .o_notebook .nav-link': '_onTabChanged', // Detectar cambio de pestaña
         }),
 
+        /**
+         * Detectar el cambio de pestaña dentro del notebook
+         */
         _onTabChanged: function (event) {
             const tab = $(event.target).attr('aria-controls');
             const tabName = tab === 'assign_tab' ? 'assign' : 'create';
 
+            // Enviar el cambio al backend
             this.trigger_up('field_changed', {
                 dataPointID: this.handle,
                 changes: { active_tab: tabName },
             });
 
-            console.log('Pestaña activa: ', tabName); // Depuración
+            console.log('Pestaña activa: ', tabName); // Mensaje de depuración
         },
     });
 });
