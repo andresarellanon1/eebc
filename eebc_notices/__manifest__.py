e795729d846dd4974f79ef3a0cc6bcc3eda66741
Aybancho <ivan.rosas@uabc.edu.mx>
2024-12-03 13:20:19 -0800

    test js cambio pestaña v10

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.81',
+    'version': '17.0.1.82',
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
     'category': 'other',
