30dac2fa1bb8a41cb69158950d2d307e331939f2
alejandro ramos <ivan.alj@hotmail.com>
2024-12-04 10:43:38 -0800

    test js v4

--- eebc_notices/static/src/js/tab_change_handler.js
+++ eebc_notices/static/src/js/tab_change_handler.js
@@ -1,55 +1,35 @@
 odoo.define('eebc_notices.NoticeFileWizardController', [
-    '@web/views/form/form_controller', '@web/views/form/form_view', '@web/core/registry'
+    '@web/views/form/form_controller',
+    '@web/views/form/form_view',
+    '@web/views/form/form_renderer',
+    '@web/core/registry',
 ], function (require) {
     'use strict';
 
     const { FormController } = require('@web/views/form/form_controller');
     const { FormView } = require('@web/views/form/form_view');
+    const { FormRenderer } = require('@web/views/form/form_renderer');
     const { registry } = require('@web/core/registry');
 
-
-
+    /**
+     * Clase personalizada para el controlador del formulario
+     */
     class NoticeFileWizardController extends FormController {
         setup() {
             console.log('Notice File Wizard Controller iniciado');
-
-            super.setup();
+            super.setup(...arguments); // Llamar al setup original
         }
     }
 
+    /**
+     * Definición de la vista personalizada
+     */
+    const noticeWizardFormView = {
+        ...FormView, // Extiende la vista del formulario original
+        Controller: NoticeFileWizardController, // Usa el controlador personalizado
+        Renderer: FormRenderer, // Usa el renderer original para la vista del formulario
+    };
 
-        const noticeWizardFormView ={
-            ...FormView,
-            Controller:NoticeFileWizardController,
-
-        }
-
-        registry.category("views").add("noticeWizardFormViewTab",noticeWizardFormView)
-    
-
-
-
-
-        // async start() {
-        //     await super.start();
-        //     console.log('Vista de modelo transitorio cargada correctamente');
-        //     this.el.querySelectorAll('.o_notebook .nav-link').forEach((element) => {
-        //         element.addEventListener('click', this._onTabChanged.bind(this));
-        //     });
-        // }
-
-        // _onTabChanged(event) {
-        //     const tab = event.currentTarget.getAttribute('aria-controls');
-        //     const tabName = tab === 'assign_tab' ? 'assign' : 'create';
-
-        //     console.log('Pestaña activa:', tabName);
-
-        //     // Actualiza el backend con la pestaña activa
-        //     this.trigger_up('field_changed', {
-        //         dataPointID: this.props.dataPointID,
-        //         changes: { active_tab: tabName },
-        //     });
-        // }
-   
-
+    // Registra la vista personalizada en el registro de vistas
+    registry.category('views').add('noticeWizardFormViewTab', noticeWizardFormView);
 });
