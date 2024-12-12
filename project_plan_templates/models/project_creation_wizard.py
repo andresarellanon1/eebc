2a714395d43728ec356d7a7324d6a60220416054
YaedQuadro <161377150+ElYaedH@users.noreply.github.com>
2024-12-12 11:06:16 -0800

    fix??

--- project_plan_templates/models/project_creation_wizard.py
+++ project_plan_templates/models/project_creation_wizard.py
@@ -57,14 +57,11 @@ class ProjectCreation(models.TransientModel):
     def action_confirm_create_project(self):
         self.ensure_one()
 
-        project_plan_lines = self.prep_plan_lines(self.sale_order_id.project_plan_lines)
-        picking_line_vals = self.prep_picking_lines(self.sale_order_id.project_picking_lines)
-
         project_vals = {
             'name': self.project_name,
             'description': self.description,
-            'project_plan_lines': self.wizard_plan_lines,
-            'project_picking_lines': self.wizard_picking_lines,
+            'project_plan_lines': self.prep_plan_lines(self.sale_order_id.project_plan_lines),
+            'project_picking_lines': self.prep_picking_lines(self.sale_order_id.project_picking_lines),
             'default_picking_type_id': self.picking_type_id.id,
             'publication_date': fields.Datetime.now(),
             'date_start': self.date_start,
