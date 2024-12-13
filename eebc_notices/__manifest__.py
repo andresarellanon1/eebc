c7ac963f5887939aa2e99d58adca680756b19f7b
alejandro ramos <ivan.alj@hotmail.com>
2024-12-13 13:28:37 -0800

    AHORA SIII, CON ESTE CAMBIO YA VAN A PODER CHAMBEAR!!!

--- eebc_notices/__manifest__.py
+++ eebc_notices/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "EBBC NOTICES",
-    'version': '17.0.1.98',
+    'version': '17.0.1.99',
     'author': "Quadro Soluciones",
     'website': 'https://quadrosoluciones.com/',
     'category': 'other',
