6267a20362b137fdf2f550435a133b13a6ee2e83
alejandro ramos <ivan.alj@hotmail.com>
2024-12-13 13:19:37 -0800

    test lot_ids en salida v4.10

--- eebc_notices/wizard/__init__.py
+++ eebc_notices/wizard/__init__.py
@@ -1,9 +1,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import notice_file_wizard
+
 from . import select_notice_wizard
 from . import wizard_selection_line
 from . import wizard_selection_lot_line
 
-from . import notice_file_wizard
 
 
 
