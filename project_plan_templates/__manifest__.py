06ff6211f2882a5d278b511e72efb73f46ee6be0
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2024-12-16 13:55:31 -0800

    ll

--- project_plan_templates/__manifest__.py
+++ project_plan_templates/__manifest__.py
@@ -24,7 +24,6 @@
         "views/inherit_project_task_inventory_view_form.xml",
         "views/task_picking_line.xml",
         "views/inherit_sale_order_view_form.xml",
-        "views/inherit_sale_project_view.xml"
     ],
     "license": "LGPL-3",
     'installable': True,
