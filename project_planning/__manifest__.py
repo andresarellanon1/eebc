fc3fac970f4ba1a762d0c2ac039ff7eb7f47437b
Ramon-hash707 <ramon@quadrosoluciones.mx>
2024-12-20 14:12:04 -0800

    Nuevo modulo planeacion de proyectos

---
+++ project_planning/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Planeación de proyectos',
+    'description': """
+            Este modulo comprende las configuraciones para la generacion de plantillas de proyectos.
+    """,
+    'version': '17.0.0.02',
+    'website': 'https://quadrosoluciones.com',
+    'author': 'Quadro Soluciones',
+    'depends': [
+                'project', 'product', 'sale', 'sale_project'
+               ],
+    'data': [
+
+    ],
+    "license": "LGPL-3",
+    'installable': True,
+    'application': False
+}
