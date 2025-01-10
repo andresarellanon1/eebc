ab6903bf165924e8ccb336d5e69a5301af193649
enriquequadro <enrique@quadrosoluciones.mx>
2025-01-10 10:28:00 -0800

    horas por jornada

--- project_plan_templates/models/account_analytic_line.py
+++ project_plan_templates/models/account_analytic_line.py
@@ -5,3 +5,8 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     estimated_time = fields.Float(string="Horas estimadas")
+    work_shift = fields.Float(string='Jornadas Laborales')
+
+    def _compute_work_hours(self):
+        for record in self:
+            record.estimated_time = record.work_shift * 8
