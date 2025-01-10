21877273de63f5cf8cc2a28a30dfde1c65f720cf
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2025-01-10 15:30:27 -0800

    Update project_creation_wizard.py

--- project_plan_templates/models/project_creation_wizard.py
+++ project_plan_templates/models/project_creation_wizard.py
@@ -104,14 +104,46 @@ class ProjectCreation(models.TransientModel):
                         current_task_type = self.get_or_create_task_type('Extras', project)
                 raise ValidationError(f"LLego al final: {project_b.id}")
                 if line.name not in existing_task_names and line.use_project_task and line.for_create:
-                    self.create_task_in_project(project, current_task_type, line)
+                    timesheet_lines = self.env['task.time.lines'].search([('task_timesheet_id', '=', line.task_timesheet_id.id)])
+                    timesheet_data = [(0, 0, {'name': ts_line.description, 'estimated_time': ts_line.estimated_time})
+                                    for ts_line in timesheet_lines]
+
+                    picking_lines = []
+                    is_task = False
+
+                    for picking in self.wizard_picking_lines:
+                        if picking.display_type:
+                            is_task = picking.name == line.name
+                        elif is_task:
+                            picking_lines.append((0, 0, {
+                                'name': picking.product_id.name,
+                                'product_id': picking.product_id.id,
+                                'product_uom': picking.product_uom.id,
+                                'product_packaging_id': picking.product_packaging_id.id,
+                                'product_uom_qty': picking.product_uom_qty,
+                                'quantity': picking.quantity,
+                                'standard_price': picking.standard_price,
+                                'subtotal': picking.subtotal,
+                                'display_type': False
+                            }))
+
+                    task_id = self.env['project.task'].create({
+                        'name': line.name,
+                        'project_id': project.id,
+                        'stage_id': current_task_type.id,
+                        'timesheet_ids': timesheet_data,
+                        'planned_date_begin': line.planned_date_begin,
+                        'date_deadline': line.planned_date_end,
+                        'project_picking_lines': picking_lines
+                    })
+
+                    self.create_project_tasks_pickings(task_id, line.project_plan_pickings.project_picking_lines)
+
             existing_pickings = self.env['stock.picking'].search([('origin', 'ilike', project.name)])
             existing_picking_names = existing_pickings.mapped('name')
             for picking_line in self.wizard_picking_lines:
                 if picking_line.name not in existing_picking_names and not picking_line.display_type:
                     self.create_project_tasks_pickings(None, [picking_line])
-
-            
 
             return {
                 'name': 'Project Version History',
@@ -128,42 +160,6 @@ class ProjectCreation(models.TransientModel):
                     'default_modification_date': fields.Datetime.now(),
                 }
             }
-
-    def create_task_in_project(self, current_task_type, project, line):
-        timesheet_lines = self.env['task.time.lines'].search([('task_timesheet_id', '=', line.task_timesheet_id.id)])
-        timesheet_data = [(0, 0, {'name': ts_line.description, 'estimated_time': ts_line.estimated_time})
-                        for ts_line in timesheet_lines]
-
-        picking_lines = []
-        is_task = False
-
-        for picking in self.wizard_picking_lines:
-            if picking.display_type:
-                is_task = picking.name == line.name
-            elif is_task:
-                picking_lines.append((0, 0, {
-                    'name': picking.product_id.name,
-                    'product_id': picking.product_id.id,
-                    'product_uom': picking.product_uom.id,
-                    'product_packaging_id': picking.product_packaging_id.id,
-                    'product_uom_qty': picking.product_uom_qty,
-                    'quantity': picking.quantity,
-                    'standard_price': picking.standard_price,
-                    'subtotal': picking.subtotal,
-                    'display_type': False
-                }))
-
-        task_id = self.env['project.task'].create({
-            'name': line.name,
-            'project_id': project.id,
-            'stage_id': current_task_type.id,
-            'timesheet_ids': timesheet_data,
-            'planned_date_begin': line.planned_date_begin,
-            'date_deadline': line.planned_date_end,
-            'project_picking_lines': picking_lines
-        })
-
-        self.create_project_tasks_pickings(task_id, line.project_plan_pickings.project_picking_lines)
 
     def create_project_tasks_pickings(self, task_id, pickings):
         for line in pickings:
