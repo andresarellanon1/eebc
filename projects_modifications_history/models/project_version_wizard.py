28431e04f5a0c02da7bf255cdc98e3dfc14fc93e
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2025-01-22 09:33:19 -0800

    Update project_version_wizard.py

--- projects_modifications_history/models/project_version_wizard.py
+++ projects_modifications_history/models/project_version_wizard.py
@@ -33,7 +33,6 @@ class ProjectVersionWizard(models.TransientModel):
     )
 
     project_id = fields.Many2one('project.project', string='Proyecto', required=True)
-
     location_id = fields.Many2one('stock.location', string='Ubicación de origen')
     location_dest_id = fields.Many2one('stock.location', string='Ubicación de destino')
     scheduled_date = fields.Datetime(string='Fecha programada de entrega')
