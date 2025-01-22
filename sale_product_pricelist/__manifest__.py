226fee196796cb2bd43eec6c14cbecf2351eefdb
jes.ale <jes@alejos.dev>
2025-01-22 04:13:54 -0800

    fix 27

--- sale_product_pricelist/__manifest__.py
+++ sale_product_pricelist/__manifest__.py
@@ -5,9 +5,17 @@
         Este módulo muestra una tabla con la lista de precios dentro de la vista de cada producto.
         Muestra una widget de selección para la lista de precios en el documento de venta y el documento de factura de cliente.
         Actualiza el precio unitario de la línea en cuestión correspondiente con la lista de precios seleccionada en cualquiera de estos dos documentos.
-        Multisucursal.
+        Este modulo implementa metodos para computar la lista de precios y el precio unitario tomando en cuenta la divisa secundaria para la empresa.
+        Seleccion default:
+            Al seleccionar el producto de la linea se implementa un algoritmo de prioridades.
+            Las prioridades se definen por la lista de precio del cliente (base de odoo) y la lista prioritaria agregada en un modulo personalizado.
+            El algoritmo encuentra la lista de precios con mayor prioridad en caso de que el producto se encuentre en mas de una.
+        Multi divisa:
+            Si la orden cambio de divisa, busca una lista de precio con el mismo nombre pero en la divisa objetivo.
+        Multi sucursal:
+            Encuentra solamente listas de precio correspondientes a la sucursal del vendedor de la orden.
     """,
-    'version': '17.0.3.002',
+    'version': '17.0.4.001',
     'website': 'https://quadrosoluciones.com',
     'author': 'Quadro Soluciones',
     'depends': [
@@ -15,7 +23,8 @@
         'product',
         'sale_management',
         'product_email_template',
-        'partner_priority_pricelist'
+        'partner_priority_pricelist',
+        'currency_lock'
         # todo: agregar moudlo de  contabilidad mexicana para depender del campod unspsc
     ],
     'data': [
--- sale_product_pricelist/models/__init__.py
+++ sale_product_pricelist/models/__init__.py
@@ -1,4 +1,6 @@
 from . import product_pricelist
 from . import product_pricelist_line
 from . import product_template
+from . import res_config_settings
+from . import sale_order
 from . import sale_order_line
