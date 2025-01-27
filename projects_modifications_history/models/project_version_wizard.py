01f3fc9514df2458bd2d2e02a965a89b467b3b98
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2025-01-27 09:59:03 -0800

    Update project_version_wizard.py

--- projects_modifications_history/models/project_version_wizard.py
+++ projects_modifications_history/models/project_version_wizard.py
@@ -80,27 +80,28 @@ class ProjectVersionWizard(models.TransientModel):
         new_plan_lines_data = self.prep_plan_lines(self.sale_order_id.project_plan_lines)
 
         project.project_plan_lines = [
-            (1, line.id, new_line[2]) if line.name == new_line[2]['name'] else (4, line.id)
+            (1, line[1], new_line[2]) if line[2]['name'] == new_line[2]['name'] else (4, line[1])
             for line in existing_plan_lines
             for new_line in new_plan_lines_data
-            if line.name == new_line[2]['name']
+            if line[2]['name'] == new_line[2]['name']
         ] + [
             new_line for new_line in new_plan_lines_data
-            if all(new_line[2]['name'] != line.name for line in existing_plan_lines)
+            if all(new_line[2]['name'] != line[2]['name'] for line in existing_plan_lines)
         ]
 
         existing_picking_lines = self.prep_picking_lines(project.project_picking_lines)
         new_picking_lines_data = self.prep_picking_lines(self.sale_order_id.project_picking_lines)
 
         project.project_picking_lines = [
-            (1, line.id, new_line[2]) if line.name == new_line[2]['name'] else (4, line.id)
+            (1, line[1], new_line[2]) if line[2]['name'] == new_line[2]['name'] else (4, line[1])
             for line in existing_picking_lines
             for new_line in new_picking_lines_data
-            if line.name == new_line[2]['name']
+            if line[2]['name'] == new_line[2]['name']
         ] + [
             new_line for new_line in new_picking_lines_data
-            if all(new_line[2]['name'] != line.name for line in existing_picking_lines)
+            if all(new_line[2]['name'] != line[2]['name'] for line in existing_picking_lines)
         ]
+
         project.write({})
 
         # Check if a version history already exists for the current project.
