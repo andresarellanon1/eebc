4aa24963f25923e7f1a0afc10d718361184021e5
Quadro Soluciones Teconologicas <quadro@Quadro.quadrosoluciones.com>
2025-01-27 12:59:33 -0800

    Commit transaction

--- sale_product_pricelist/models/sale_order_line.py
+++ sale_product_pricelist/models/sale_order_line.py
@@ -94,12 +94,16 @@ class SaleOrderLine(models.Model):
         """
         for line in self:
             if line.product_id and (line.product_pricelist_id.currency_id.id != line.order_id.target_currency_id.id):
+                self.env.cr.commit()  # Save changes made to the parent order before continue
                 product_pricelist = self.env["product.pricelist.line"].search([
                     ("product_templ_id", "=", line.product_template_id.id),
                     ("name", "=", line.product_pricelist_id.name),
                     ("currency_id", "=", line.order_id.target_currency_id.id),
                     ("company_id", "=", line.order_id.company_id.id)
                 ], limit=1)
+                logger.warning(f"== {line.product_template_id.name} ==")
+                logger.warning(f"== {line.order_id.target_currency_id.name} ==")
+                logger.warning(f"== {line.order_id.company_id.name} ==")
                 logger.warning(f"== {product_pricelist.display_name} ==")
                 if product_pricelist:
                     line.product_pricelist_id = product_pricelist
