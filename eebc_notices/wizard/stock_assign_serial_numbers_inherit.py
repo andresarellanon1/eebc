de132356c3bc7adb76744fe2ba8ed1e9d50646f6
Aybancho <ivan.rosas@uabc.edu.mx>
2025-01-28 10:07:10 -0800

    test generacion de serie/lote v1

--- eebc_notices/wizard/stock_assign_serial_numbers_inherit.py
+++ eebc_notices/wizard/stock_assign_serial_numbers_inherit.py
@@ -26,5 +26,21 @@ class StockAssignSerialNumbers(models.TransientModel):
 
     def generate_serial_numbers(self):
         self.ensure_one()
+        if self.is_aviso:
+            # Validar que no existan números de serie duplicados para este producto
+            existing_lots = self.env['stock.lot'].search([
+                ('product_id', '=', self.move_id.product_id.id),
+                ('company_id', '=', self.env.company.id),
+            ]).mapped('name')
+
+            # Generar los nombres de los lotes
+            lot_names = self.env['stock.lot'].generate_lot_names(self.next_serial_number, self.next_serial_count)
+
+            # Validar duplicados
+            for lot in lot_names:
+                if lot['lot_name'] in existing_lots:
+                    raise ValidationError(_(
+                        "El número de serie/lote '%s' ya existe para el producto '%s'."
+                    ) % (lot['lot_name'], self.move_id.product_id.display_name))    
         self.move_id.next_serial = self.next_serial_number or ""
         return self.move_id._generate_serial_numbers(self.next_serial_number, next_serial_count=self.next_serial_count)
