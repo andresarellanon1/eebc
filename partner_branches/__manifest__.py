4e401d570b95563d657051cc00dfebb94be5724e
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-01-29 13:15:00 -0800

    update partner_branches

--- partner_branches/__manifest__.py
+++ partner_branches/__manifest__.py
@@ -1,5 +1,11 @@
 {
     "name": "Partner Branches",
+    'summary': 'Permite gestionar contactos como sucursales, facilitando la organización y el manejo de información de sucursales bajo una entidad principal.',
+    'description': """
+        Extiende la funcionalidad de los contactos en Odoo para permitir la gestión de sucursales, incluyendo 
+        la asignación de nombres, números y relaciones jerárquicas. Facilita la organización de contactos que 
+        representan sucursales de una entidad principal.
+    """,
     "version": "17.0.3.001",
     "depends": [
         "stock",
@@ -8,9 +14,6 @@
     "author": "Quadro Soluciones",
     "website": "https://quadrosoluciones.com/",
     "category": "other",
-    "description": """
-        Contactos como sucursales.
-    """,
     "data": [
         "views/res_partner.xml"
     ],
