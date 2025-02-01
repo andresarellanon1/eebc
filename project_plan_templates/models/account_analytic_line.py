9b6a76ae4ca64e030f0257e8908da01cfe3845c6
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2025-01-31 16:16:12 -0800

    horas estimadas dinamicas

--- project_plan_templates/models/account_analytic_line.py
+++ project_plan_templates/models/account_analytic_line.py
@@ -4,10 +4,10 @@ class AccountAnalyticLine(models.Model):
 
     _inherit = 'account.analytic.line'
 
-    estimated_time = fields.Float(string="Horas estimadas")
+    estimated_time = fields.Float(string="Horas estimadas", compute="_compute_estimated_time", store=True)
     work_shift = fields.Float(string='Jornadas Laborales')
 
-    @api.onchange('work_shift')
-    def _work_shift_onchange_(self):
+    @api.depends('work_shift')
+    def _compute_estimated_time(self):
         for record in self:
             record.estimated_time = record.work_shift * 8
