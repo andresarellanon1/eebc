c24061d551c5196932576b0d09a11c52da772461
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-07 11:23:45 -0800

    update

--- sale_product_pricelist/models/product_pricelist_line.py
+++ sale_product_pricelist/models/product_pricelist_line.py
@@ -84,6 +84,9 @@ class ProductPricelistLine(models.Model):
             else:
                 record.display_name = record.name
             
+            if record.display_name == False:
+                record.unlink()
+
             logger.warning(
                 "Pricelist_id: %s - Display_name: ID %s - Name: %s - Display Name: %s", 
                 record.pricelist_id,
