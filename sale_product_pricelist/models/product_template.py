53e8e56ef57c72a6135bde1f2d6724ea5aaa92e8
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-07 11:09:45 -0800

    update

--- sale_product_pricelist/models/product_template.py
+++ sale_product_pricelist/models/product_template.py
@@ -58,26 +58,33 @@ class ProductTemplate(models.Model):
                 })
 
                 # Log antes de realizar cualquier acción para ver el estado actual de las líneas de la lista de precios
-                logger.warning(f"Antes de la eliminación - Líneas de precios actuales: {product_template.product_pricelist_line_ids}")
+                logger.warning(f"Antes de la eliminación - Líneas de precios actuales:")
+                for line in product_template.product_pricelist_line_ids:
+                    logger.warning(f"  - {line.name}")  # Muestra el nombre de cada línea
 
                 # 2. Eliminar todas las líneas existentes
                 product_template.sudo().write({'product_pricelist_line_ids': [(5, 0, 0)]})
 
                 # Log después de eliminar las líneas para asegurarse de que se han eliminado
-                logger.warning(f"Después de la eliminación - Líneas de precios: {product_template.product_pricelist_line_ids}")
+                logger.warning(f"Después de la eliminación - Líneas de precios:")
+                for line in product_template.product_pricelist_line_ids:
+                    logger.warning(f"  - {line.name}")  # Debería estar vacío si se eliminaron correctamente
 
                 # 3. Crear nuevas líneas
                 if pricelist_line_vals:
                     product_template.sudo().write({'product_pricelist_line_ids': [(0, 0, vals) for vals in pricelist_line_vals]})
 
                     # Log después de agregar las nuevas líneas para verificar que se han creado correctamente
-                    logger.warning(f"Después de la creación - Nuevas líneas de precios: {product_template.product_pricelist_line_ids}")
+                    logger.warning(f"Después de la creación - Nuevas líneas de precios:")
+                    for line in product_template.product_pricelist_line_ids:
+                        logger.warning(f"  - {line.name}")  # Muestra el nombre de cada nueva línea creada
                 else:
                     # Si no hay nuevas líneas, escribir `False`
                     product_template.sudo().write({'product_pricelist_line_ids': False})
 
                     # Log si no hay líneas nuevas para asociar
                     logger.warning(f"No se crearon nuevas líneas. Campo 'product_pricelist_line_ids' set a False.")
+
 
     def get_min_sale_price(self, currency_id):
         """
