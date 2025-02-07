bfbfbe5f60fafbe0ca05e529cbf7a74a013a2908
ElYaedH <161377150+ElYaedH@users.noreply.github.com>
2025-02-07 15:59:51 -0800

    Update project_version_wizard.py

--- projects_modifications_history/models/project_version_wizard.py
+++ projects_modifications_history/models/project_version_wizard.py
@@ -98,8 +98,6 @@ class ProjectVersionWizard(models.TransientModel):
         # Validar que se haya proporcionado un motivo de modificación
         if not self.modification_motive:
             raise UserError('Hace falta agregar el motivo de la modificación.')
-        
-        self.sale_order_id.change_for_modification()
 
         # Crear tareas para el proyecto
         project.create_project_tasks(self.location_id.id, self.location_dest_id.id, self.scheduled_date)
