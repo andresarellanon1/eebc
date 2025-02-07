a81a833ad0c162e95c89efbd59fb477fed36d0e4
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-07 12:39:20 -0800

    update

--- sale_product_pricelist/models/product_pricelist.py
+++ sale_product_pricelist/models/product_pricelist.py
@@ -60,9 +60,8 @@ class ProductPricelist(models.Model):
         for pricelist in self:
             pricelist_lines = self.env['product.pricelist.line'].search([('pricelist_id', '=', pricelist.id)])
     
-            # Imprimir el name y is_orphan de cada línea
-            # for line in pricelist_lines:
-            #     logger.warning(f"Línea de lista de precios: {line.name}, ¿Es huérfana?: {line.is_orphan}")
+            for line in pricelist_lines:
+                line.def _compute_display_name()
 
             items_direct_relation_variant = self.env['product.pricelist.item'].search([('applied_on', '=', '0_product_variant'), ('pricelist_id', '=', pricelist.id)])
             items_direct_relation = self.env['product.pricelist.item'].search([('applied_on', '=', '1_product'), ('pricelist_id', '=', pricelist.id)])
