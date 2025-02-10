0f28d94b6d0958a825dd216aba39683e0e17d1ff
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-10 14:48:46 -0800

    update

--- sale_product_pricelist/models/product_template.py
+++ sale_product_pricelist/models/product_template.py
@@ -36,13 +36,11 @@ class ProductTemplate(models.Model):
     @api.depends_context('company')
     @api.depends("categ_id", "list_price", "standard_price")
     def _compute_product_pricelist_line_ids(self):
-        logger.warning('Inicio _compute_product_pricelist_line_ids')
 
         # 1. Obtener y eliminar todas las líneas de precios en una sola operación
         all_pricelist_lines = self.mapped("product_pricelist_line_ids")
         if all_pricelist_lines:
             all_pricelist_lines.unlink()
-        logger.warning('Se eliminaron todas las listas de precios')
 
         # 2. Preparar un diccionario para almacenar las líneas de precios por producto
         pricelist_lines_by_product = defaultdict(list)
@@ -69,59 +67,6 @@ class ProductTemplate(models.Model):
 
         # 4. Recalcular los orphans en bloque
         self.mapped("product_pricelist_line_ids")._compute_is_orphan()
-
-        logger.warning('Termino _compute_product_pricelist_line_ids')
-
-
-
-    # @api.depends_context('company')
-    # @api.depends("categ_id", "list_price", "standard_price")
-    # def _compute_product_pricelist_line_ids(self):
-    #     logger.warning('Inicio _compute_product_pricelist_line_ids')
-    #     """
-    #         Re-computes the price unit for all the 'product.pricelist.line' linked to this 'product.template'.
-    #         Search for pricelist items applied on:
-    #             - this product template id
-    #             - this product template related category id
-    #             - globally applied items
-    #         and aggregates them.
-    #         Get the price for each pricelist and create/update the lines for the template.
-    #         Finally, re-computes the related o2m field for this 'product.template'.
-    #     """
-    #     # Obtener todas las líneas de precios en un solo paso y eliminarlas
-    #     all_pricelist_lines = self.mapped("product_pricelist_line_ids")
-    #     if all_pricelist_lines:
-    #         all_pricelist_lines.unlink()
-    #     logger.warning('Se eliminaron todas las listas de precios')
-
-
-    #     for product_template in self:
-    #         pricelist_line_vals = []
-    #         applied_pricelists = product_template._get_include_template_pricelist_ids()
-    #         # 1. Prepare vals for new lines
-    #         for pricelist in applied_pricelists:
-    #             pricelist_line_vals.append({
-    #                 'name': pricelist.name,
-    #                 'pricelist_id': pricelist.id,
-    #                 'uom_id': product_template.uom_id.id,
-    #                 'product_templ_id': product_template.id,
-    #                 'currency_id': pricelist.currency_id.id,
-    #                 'company_id': pricelist.company_id.id,
-    #                 'is_special': pricelist.is_special
-    #             })
-
-    #         # 2. Unlink and delete all
-    #         # product_template.sudo().write({'product_pricelist_line_ids': [(5, 0, 0)]})
-    #         # 3. Link and create all
-    #         # self.env.cr.commit()
-    #         product_template.sudo().write({'product_pricelist_line_ids': [(0, 0, vals) for vals in pricelist_line_vals]})
-    #         # 4. recompute orphans
-    #         product_template.product_pricelist_line_ids._compute_is_orphan()
-    #         # If nothing to link, write to `False`
-    #         if len(pricelist_line_vals) <= 0:
-    #             product_template.sudo().write({'product_pricelist_line_ids': False})
-
-    #     logger.warning('Termino _compute_product_pricelist_line_ids')
 
     def get_min_sale_price(self, currency_id):
         """
