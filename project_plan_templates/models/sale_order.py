1c37597d83667bab57a82a0c95bd3551cefba1b4
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-12 13:28:44 -0800

    update

--- project_plan_templates/models/sale_order.py
+++ project_plan_templates/models/sale_order.py
@@ -283,20 +283,15 @@ class SaleOrder(models.Model):
                         'last_service_price': line.last_service_price
                     }) for line in previous_order.order_line]
 
-                    # Copiar líneas de plan
-                    sale.project_plan_lines = self._prepare_plan_lines(previous_order.project_plan_lines)
-
-                    # Copiar líneas de picking
-                    sale.project_picking_lines = self._prepare_picking_lines(previous_order.project_picking_lines)
-
-                    # Copiar líneas de tareas
-                    sale.task_time_lines = self._prepare_task_lines(previous_order.task_time_lines)
-
                     _logger.warning(f"Se copiaron {len(previous_order.order_line)} líneas de orden al nuevo pedido")
 
                     # Copiar líneas de plan
                     _logger.warning(f"Copiando líneas de planificación del pedido anterior: {previous_order.name}")
                     sale.project_plan_lines = self._prepare_plan_lines(previous_order.project_plan_lines)
+
+                    for plan_line in sale.project_plan_lines:
+                        _logger.warning(f"Plan Line: {plan_line} | Nombre: {plan_line.name} | price_unit: {plan_line.price_unit}")
+
                     _logger.warning(f"Se copiaron {len(previous_order.project_plan_lines)} líneas de planificación")
 
                     # Copiar líneas de picking
