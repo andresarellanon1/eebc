cb266f7dd61cf33269936ed4b91e570c8f41fa59
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-12 13:00:51 -0800

    update

--- project_plan_templates/models/sale_order.py
+++ project_plan_templates/models/sale_order.py
@@ -204,11 +204,16 @@ class SaleOrder(models.Model):
                 """
                 Elimina las lineas que ya han sido modificadas para reemplazarlas
                 """
-                plannig_lines_to_remove = sale.project_plan_lines.filtered(lambda line: not line.for_modification)
-                plannig_lines_to_remove.unlink()
+                # plannig_lines_to_remove = sale.project_plan_lines.filtered(lambda line: not line.for_modification)
+                # plannig_lines_to_remove.unlink()
+                # time_lines_to_remove = sale.task_time_lines.filtered(lambda line: not line.for_modification)
+                # time_lines_to_remove.unlink()
+                # picking_lines_to_remove = sale.project_picking_lines.filtered(lambda line: not line.for_modification)
+                # picking_lines_to_remove.unlink() 
+
+                sale.project_plan_lines.unlink()
+                sale.project_picking_lines.unlink()
                 sale.task_time_lines.unlink()
-                picking_lines_to_remove = sale.project_picking_lines.filtered(lambda line: not line.for_modification)
-                picking_lines_to_remove.unlink() 
 
                 plan_pickings = []
                 existing_lines = {line.name: line.sequence for line in sale.project_plan_lines}
