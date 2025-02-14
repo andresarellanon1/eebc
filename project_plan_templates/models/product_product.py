af1dd82c8d16368416fc270107102c7009da48b2
Ramon-hash707 <ramon@quadrosoluciones.mx>
2025-02-14 10:38:18 -0800

    fix

--- project_plan_templates/models/product_product.py
+++ project_plan_templates/models/product_product.py
@@ -5,4 +5,3 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     project_quantity = fields.Integer(string="Cantidad en el proyecto")
-    is_extra = fields.Boolean(string="Material extra", defaut=False)
